3abc6e8f9eba37a122d7c4ada1e6746bdf0ba291
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-04 16:41:48 -0300

    ajustes no menu de pf

--- perfil/includes/menu_interno_pf.php
+++ perfil/includes/menu_interno_pf.php
@@ -4,11 +4,10 @@
 			<strong>
 			| <a href="?perfil=informacoes_iniciais_pf">Informações Iniciais</a>
 			| <a href="?perfil=endereco_pf">Endereço</a>
-			| <a href="?perfil=arquivos_pf">Anexos</a>
+			| <a href="?perfil=arquivos_pf">Arquivos Pessoais</a>
 			| <a href="?perfil=projeto">Projeto</a>
-			| <a href="?perfil=informacoes_administrativas_pf">Informações Administrativas</a>
 			<?php
-			if(isset($_SESSION['idEvento']))
+			if(isset($_SESSION['idProjeto']))
 			{
 			?>
 				| <a href="?perfil=evento_edicao">Evento</a>
@@ -16,6 +15,7 @@
 			<?php
 			}
 			?>
+			| <a href="?perfil=informacoes_administrativas_pf">Informações Administrativas</a><br/>
 			| <a href="?secao=perfil">Início</a>
 			| <a href="../manual" target="_blank">Ajuda</a>
 			| <a href="../include/logoff.php">Sair</a> |</strong><br/>
