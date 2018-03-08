dbeee5cf63e607a493bd7b716935b7539421c62a
LoreleiGab <loreleigab@hotmail.com>
2018-03-08 17:11:27 -0300

    Tela de cadastro para novo projeto

--- perfil/projeto_novo.php
+++ perfil/projeto_novo.php
@@ -1,135 +1,173 @@
 <?php
 $con = bancoMysqli();
-$idPf= $_SESSION['idUser'];
+
+if(isset($_POST['novoPj'])) //tipoePessoa = 2
+{
+	$idPj = $_SESSION['idUser'];
+	$idAreaAtuacao = $_POST['idAreaAtuacao'];
+	if(isset($_POST['contratoGestao']))
+	{
+		$contratoGestao = $_POST['contratoGestao'];
+	}
+	else
+	{
+		$contratoGestao = 0;
+	}
+	$sql_insere_projeto = "INSERT INTO projeto (idPj, contratoGestao, tipoPessoa, idAreaAtuacao, publicado) VALUES ('$idPj', '$contratoGestao', 2, '$idAreaAtuacao', 1)";
+	if(mysqli_query($con,$sql_insere_projeto))
+	{
+		$sql_ultimo = "SELECT idProjeto FROM projeto ORDER BY idProjeto DESC LIMIT 0,1";
+		$query_ultimo = mysqli_query($con,$sql_ultimo);
+		$ultimoProjeto = mysqli_fetch_array($query_ultimo);
+		$_SESSION['idProjeto']  = $ultimoProjeto['idProjeto'];
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso! Aguarde...</strong></font>";
+		echo "<meta HTTP-EQUIV='refresh' CONTENT='0.5;URL=?perfil=projeto_2'>";
+	}
+	else
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Erro ao gravar! Tente novamente.</strong></font> <br/>";
+	}
+}
+
+if(isset($_POST['insereAtuacao']))
+{
+	$idPf = $_SESSION['idUser'];
+	$idAreaAtuacao = $_POST['idAreaAtuacao'];
+	$sql_insere_projeto = "INSERT INTO projeto (idPf, tipoPessoa, idAreaAtuacao, publicado) VALUES ('$idPf', 1, '$idAreaAtuacao', 1)";
+	if(mysqli_query($con,$sql_insere_projeto))
+	{
+		$sql_ultimo = "SELECT idProjeto FROM projeto ORDER BY idProjeto DESC LIMIT 0,1";
+		$query_ultimo = mysqli_query($con,$sql_ultimo);
+		$ultimoProjeto = mysqli_fetch_array($query_ultimo);
+		$_SESSION['idProjeto']  = $ultimoProjeto['idProjeto'];
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso! </strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Erro ao gravar! Tente novamente.</strong></font> <br/>";
+	}
+}
 ?>
-<section id="inserir" class="home-section bg-white">
-    <div class="container"><?php include '../perfil/includes/menu_interno_pf.php'; ?>
+<section id="list_items" class="home-section bg-white">
+    <div class="container">
+    	<?php
+    	if($_SESSION['tipoPessoa'] == 1)
+		{
+			$idPf= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pf.php';
+			$pf = recuperaDados("pessoa_fisica","idPf",$idPf);
+			$cooperado = $pf['cooperado'];
+		}
+		else
+		{
+			$idPj= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pj.php';
+		}
+    	?>
 		<div class="form-group">
-			<h4>PASSSO 1:</h4>
+			<h4>Cadastro de Projeto</h4>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
-				<form method="POST" action="?perfil=evento_edicao" class="form-horizontal" role="form">
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Área de atuação *</label>
-							<select class="form-control" name="idAreaAtuacao" >
-								<option value="1"></option>
-								<?php echo geraOpcao("area_atuacao","") ?>
-							</select>
+				<?php
+				if($_SESSION['tipoPessoa'] == 2) //Pessoa Jurídica
+				{
+				?>
+					<form method="POST" action="?perfil=projeto_novo" class="form-horizontal" role="form">
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8">
+								<strong>Possui Contrato de Gestão ou Termo de Colaboração com o Poder Público?* </strong>&nbsp;&nbsp;&nbsp;<input type="checkbox" name="contratoGestao" value="1">
+							</div>
 						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-3 col-md-2">
-							<label>Valor total do <br/>projeto</label>
-							<input type="text" name="nomeGrupo" class="form-control" maxlength="100" id="inputSubject" placeholder="Nome do coletivo, grupo teatral, etc." />
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8">
+								<label>Área de atuação *</label>
+								<select class="form-control" name="idAreaAtuacao" >
+									<option value="1"></option>
+									<?php echo geraOpcao("area_atuacao","") ?>
+								</select>
+							</div>
 						</div>
-						<div class="col-md-2">
-							<label>Valor solicitado como incentivo</label>
-							<input type="text" name="nomeGrupo" class="form-control" maxlength="100" id="inputSubject" placeholder="Nome do coletivo, grupo teatral, etc." />
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8">
+								<input type="submit" name="novoPj" class="btn btn-theme btn-md btn-block" value="gravar">
+							</div>
 						</div>
-						<div class="col-md-2">
-							<label>Valor de outros financiamentos</label>
-							<input type="text" name="nomeGrupo" class="form-control" maxlength="100" id="inputSubject" placeholder="Nome do coletivo, grupo teatral, etc." />
+					</form>
+				<?php
+				}
+				elseif($_SESSION['tipoPessoa'] == 1 && $cooperado = 1) //Pessoa Física Cooperado
+				{
+					if(!isset($_SESSION['idProjeto']))
+					{
+				?>
+						<div class="form-group">
+							<form method="POST" action="?perfil=projeto_novo" class="form-horizontal" role="form">
+								<div class="col-md-offset-2 col-md-5">
+									<label>Área de atuação *</label><br/><?php var_dump($pf) ?>
+									<select class="form-control" name="idAreaAtuacao" >
+										<option value="1"></option>
+										<?php echo geraOpcao("area_atuacao","") ?>
+									</select>
+								</div>
+								<div class="col-md-3"><label>&nbsp;</label><br/>
+									<input type="submit" name="insereAtuacao" class="btn btn-theme btn-md btn-block" value="Inserir">
+								</div>
+							</form>
 						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-7">
-							<label>Enquadramento da renúncia fiscal *</label>
-							<select class="form-control" name="idAreaAtuacao" >
-								<option value="1"></option>
-								<?php echo geraOpcao("area_atuacao","") ?>
-							</select>
+					<?php
+					}
+					if(isset($_SESSION['idProjeto']))
+					{
+					?>
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8">
+								<strong>Qual cooperativa está representando?* </strong>
+								<p><strong>Insira o CNPJ</strong></p>
+								</div>
+							</div>
+							<div class="form-group">
+								<form method="POST" action="?perfil=cooperativa_resultado_busca" class="form-horizontal" role="form">
+									<div class="col-md-offset-4 col-md-3">
+										<input type="text" name="busca" class="form-control" placeholder="CNPJ" id="cnpj" >
+									</div>
+									<div class="col-md-2">
+										<input type="submit" name="pesquisar" class="btn btn-theme btn-md btn-block" value="Pesquisar">
+									</div>
+								</form>
+							</div>
 						</div>
-						<div class="col-md-1"><br/>? Ícone
+					<?php
+					}
+				}
+				else
+				{
+					if($_SESSION['tipoPessoa'] == 1 && $cooperado = 0 && !isset($_SESSION['idProjeto']))
+					{
+				?>
+						<div class="form-group">
+							<form method="POST" action="?perfil=projeto_novo" class="form-horizontal" role="form">
+								<div class="col-md-offset-2 col-md-5">
+									<label>Área de atuação ****</label><br/><?php echo $cooperado ?>
+									<select class="form-control" name="idAreaAtuacao" >
+										<option value="1"></option>
+										<?php echo geraOpcao("area_atuacao","") ?>
+									</select>
+								</div>
+								<div class="col-md-3"><label>&nbsp;</label><br/>
+									<input type="submit" name="insereAtuacao" class="btn btn-theme btn-md btn-block" value="Inserir">
+								</div>
+							</form>
 						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Resumo do projeto*</label>
-							<textarea name="resumoProjeto" class="form-control" rows="10" maxlength="500"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Currículo do proponente*</label>
-							<p>Experiências na área, parcerias anteriores</p>
-							<textarea name="curriculo" class="form-control" rows="10" maxlength="25000"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Descrição do Objeto*</label>
-							<p>Considerando a realidade no qual está inserido, devendo ser demonstrado o nexo entre essa realidade e as atividades ou projetos e metas a serem atingidas.</p>
-							<textarea name="descricao" class="form-control" rows="10"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Justificativa do projeto*</label>
-							<p>Considerando a realidade no qual está inserido, devendo ser demonstrado o nexo entre essa realidade e as atividades ou projetos e metas a serem atingidas.</p>
-							<textarea name="justificativa" class="form-control" rows="10" maxlength="10000"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Objetivos e metas*</label>
-							<p>Considerando a realidade no qual está inserido, devendo ser demonstrado o nexo entre essa realidade e as atividades ou projetos e metas a serem atingidas.</p>
-							<textarea name="objetivo" class="form-control" rows="10" maxlength="10000"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Metodologia*</label>
-							<p>Parâmetros a serem utilizados para aferição do cumprimento das metas.</p>
-							<textarea name="metodologia" class="form-control" rows="10" maxlength="10000"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Local de realização*</label>
-							<p>Fazer botão.</p>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Público alvo*</label>
-							<textarea name="publicoAlvo" class="form-control" rows="10"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Plano de divulgação*</label>
-							<textarea name="planoDivulgacao" class="form-control" rows="10" maxlength="15000"></textarea>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<label>Ficha técnica*</label>
-							<p>Fazer botão.</p>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<input type="hidden" name="insere" />
-							<input type="submit" class="btn btn-theme btn-lg btn-block" value="Gravar">
-						</div>
-					</div>
-				</form>
+				<?php
+					}
+					else
+					{
+						echo "<meta HTTP-EQUIV='refresh' CONTENT='0.5;URL=?perfil=projeto_2'>";
+					}
+				}
+				?>
 			</div>
 		</div>
 	</div>
