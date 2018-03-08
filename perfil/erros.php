511f1db2c9a699b8be8bde9a5d1533f501f2a955
LoreleiGab <loreleigab@hotmail.com>
2018-03-08 16:13:19 -0300

    Tela de erro da busca do cnpj da cooperativa

--- perfil/erros.php
+++ perfil/erros.php
@@ -29,6 +29,23 @@ switch($p)
 	<?php
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=representante_pj'>";
 	break;
-
+	case 'erro_cooperativa':
+	?>
+		<section id="list_items" class="home-section bg-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-offset-2 col-md-8">
+						<div class="section-heading">
+							<h4><font color='red'>CNPJ inválido! por favor, insira o número correto!</font></h4>
+							<h4><font color='red'>Redirecionando...</font></h4>
+							<p></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+<?php
+		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=projeto_novo'>";
+	break;
 } //fim da switch
 ?>
