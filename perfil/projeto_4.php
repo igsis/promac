db620e128ec6f2a57ce75bc27e580bbfddd6af0f
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-09 08:17:00 -0300

    Tela de descrição do objeto

---
+++ perfil/projeto_4.php
@@ -0,0 +1,67 @@
+<?php
+$con = bancoMysqli();
+$idProjeto = $_SESSION['idProjeto'];
+
+if(isset($_POST['insere']))
+{
+	$descricao = $_POST['descricao'];
+
+	$sql_insere = "UPDATE projeto SET
+		descricao = '$descricao'
+		WHERE idProjeto = '$idProjeto'";
+	if(mysqli_query($con,$sql_insere))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}
+}
+
+
+$projeto = recuperaDados("projeto","idProjeto",$idProjeto);
+?>
+<section id="list_items" class="home-section bg-white">
+    <div class="container">
+    	<?php
+    	if($_SESSION['tipoPessoa'] == 1)
+		{
+			$idPf= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pf.php';
+			$pf = recuperaDados("pessoa_fisica","idPf",$idPf);
+			$cooperado = $pf['cooperado'];
+		}
+		else
+		{
+			$idPj= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pj.php';
+		}
+    	?>
+		<div class="form-group">
+			<h4>Cadastro de Projeto</h4>
+			<p><strong><?php if(isset($mensagem)){echo $mensagem;} ?></strong></p>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<form method="POST" action="?perfil=projeto_4" class="form-horizontal" role="form">
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<label>Descrição do objeto*</label>
+							<p align="justify">Considerando a realidade no qual está inserido, devendo ser demonstrado o nexo entre essa realidade e as atividades ou projetos e metas a serem atingias.</p>
+							<textarea name="descricao" class="form-control" rows="25"><?php echo $projeto['descricao'] ?></textarea>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<input type="submit" name="insere" class="btn btn-theme btn-lg btn-block" value="Gravar">
+						</div>
+					</div>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</section>
