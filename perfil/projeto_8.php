1ff1b3286ebe511376d1bd92d1f2e35b2635aadc
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-09 09:20:09 -0300

    Tela público alvo e plano e divulgação

---
+++ perfil/projeto_8.php
@@ -0,0 +1,73 @@
+<?php
+$con = bancoMysqli();
+$idProjeto = $_SESSION['idProjeto'];
+
+if(isset($_POST['insere']))
+{
+	$publicoAlvo = $_POST['publicoAlvo'];
+	$planoDivulgacao = $_POST['planoDivulgacao'];
+
+	$sql_insere = "UPDATE projeto SET
+		publicoAlvo = '$publicoAlvo',
+		planoDivulgacao = '$planoDivulgacao'
+		WHERE idProjeto = '$idProjeto'";
+	if(mysqli_query($con,$sql_insere))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}
+}
+
+
+$projeto = recuperaDados("projeto","idProjeto",$idProjeto);
+?>
+<section id="list_items" class="home-section bg-white">
+    <div class="container">
+    	<?php
+    	if($_SESSION['tipoPessoa'] == 1)
+		{
+			$idPf= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pf.php';
+		}
+		else
+		{
+			$idPj= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pj.php';
+		}
+    	?>
+		<div class="form-group">
+			<h4>Cadastro de Projeto</h4>
+			<p><strong><?php if(isset($mensagem)){echo $mensagem;} ?></strong></p>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<form method="POST" action="?perfil=projeto_8" class="form-horizontal" role="form">
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<label>Público alvo *</label>
+							<textarea name="publicoAlvo" class="form-control" rows="10"><?php echo $projeto['publicoAlvo'] ?></textarea>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<label>Plano de divulgação*</label>
+							<textarea name="planoDivulgacao" class="form-control" rows="10"><?php echo $projeto['planoDivulgacao'] ?></textarea>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<input type="submit" name="insere" class="btn btn-theme btn-lg btn-block" value="Gravar">
+						</div>
+					</div>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</section>
