3b1b007c5bdb27a04692a89075cbffbb78113086
LoreleiGab <loreleigab@hotmail.com>
2018-03-09 12:50:42 -0300

    Área de local

--- perfil/local.php
+++ perfil/local.php
@@ -2,6 +2,61 @@
 $con = bancoMysqli();
 $idProjeto = $_SESSION['idProjeto'];
 
+if(isset($_POST['insereLocal']))
+{
+	$local = $_POST['local'];
+	$estimativaPublico = $_POST['estimativaPublico'];
+	$idZona = $_POST['idZona'];
+
+	$sql_insere_local = "INSERT INTO `locais_realizacao`(`idProjeto`, `local`, `estimativaPublico`, `idZona`, `publicado`) VALUES ('$idProjeto', '$local', '$estimativaPublico', '$idZona', 1)";
+
+	if(mysqli_query($con,$sql_insere_local))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}
+}
+
+if(isset($_POST['editaLocal']))
+{
+	$idLocaisRealizacao = $_POST['editaLocal'];
+	$local = $_POST['local'];
+	$estimativaPublico = $_POST['estimativaPublico'];
+	$idZona = $_POST['idZona'];
+
+	$sql_edita_local = "UPDATE `locais_realizacao` SET
+	`local`= '$local',
+	`estimativaPublico`= '$estimativaPublico',
+	`idZona`= '$idZona'
+	WHERE idLocaisRealizacao = 'idLocaisRealizacao'";
+	if(mysqli_query($con,$sql_edita_local))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}
+}
+
+if(isset($_POST['apagaLocal']))
+{
+	$idLocaisRealizacao = $_POST['apagaLocal'];
+
+	$sql_apaga_local = "UPDATE `locais_realizacao` SET publicado = 0 WHERE idLocaisRealizacao = '$idLocaisRealizacao'";
+	if(mysqli_query($con,$sql_apaga_local))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Apagado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao apagar! Tente novamente.</strong></font>";
+	}
+}
+
 ?>
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include '../perfil/includes/menu_interno_pf.php'; ?>
@@ -48,17 +103,17 @@ $idProjeto = $_SESSION['idProjeto'];
 									echo "<tr>";
 									echo "<td class='list_description'>".$campo['local']."</td>";
 									echo "<td class='list_description'>".$campo['estimativaPublico']."</td>";
-									echo "<td class='list_description'>".retornaTipo($campo['idZona'])."</td>";
+									echo "<td class='list_description'>".$campo['idZona']."</td>";
 									echo "<td class='list_description'>
 											<form method='POST' action='?perfil=local_edicao'>
-												<input type='hidden' name='editar' value='".$campo['idLocal']."' />
-												<input type ='submit' class='btn btn-theme btn-block' value='carregar'>
+												<input type='hidden' name='editarLocal' value='".$campo['idLocaisRealizacao']."' />
+												<input type ='submit' class='btn btn-theme btn-block' value='editar'>
 											</form>
 										</td>";
 									echo "
 											<td class='list_description'>
 												<form method='POST' action='?perfil=local'>
-													<input type='hidden' name='apagar' value='".$campo['idLocal']."' />
+													<input type='hidden' name='apagaLocal' value='".$campo['idLocaisRealizacao']."' />
 													<input type ='submit' class='btn btn-theme  btn-block' value='apagar'>
 												</form>
 											</td>";
---
+++ perfil/local_novo.php
@@ -0,0 +1,58 @@
+<?php
+$con = bancoMysqli();
+$idProjeto = $_SESSION['idProjeto'];
+
+$projeto = recuperaDados("projeto","idProjeto",$idProjeto);
+?>
+<section id="list_items" class="home-section bg-white">
+    <div class="container">
+    	<?php
+    	if($_SESSION['tipoPessoa'] == 1)
+		{
+			$idPf= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pf.php';
+		}
+		else
+		{
+			$idPj= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pj.php';
+		}
+    	?>
+		<div class="form-group">
+			<h4>Cadastro de Projeto</h4>
+			<p><strong><?php if(isset($mensagem)){echo $mensagem;} ?></strong></p>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<form method="POST" action="?perfil=local" class="form-horizontal" role="form">
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-4">
+							<label>Local *</label><br/>
+							<input type="text" name="local" class="form-control" maxlength="100">
+						</div>
+
+						<div class="col-md-2"><label>Público Estimado *</label><br/>
+							<input type="text" name="estimativaPublico" class="form-control">
+						</div>
+
+						<div class="col-md-2">
+							<label>Zona *</label>
+							<select class="form-control" name="idZona" >
+								<option value="0"></option>
+								<?php echo geraOpcao("zona","") ?>
+							</select>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<input type="submit" name="insereLocal" class="btn btn-theme btn-lg btn-block" value="Inserir">
+						</div>
+					</div>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</section>
