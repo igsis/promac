f6c97e678b20882d19cb517887ed51ab7704e2d2
Diego Vicente <diego.vdossantos@gmail.com>
2018-03-09 09:54:10 -0300

    direcionamento do botão avançar

--- perfil/arquivos_pf.php
+++ perfil/arquivos_pf.php
@@ -174,7 +174,7 @@ $pf = recuperaDados("pessoa_fisica","idPf",$idPf);
 						</form>
 					</div>
 					<div class="col-md-offset-4 col-md-2">
-						<form class="form-horizontal" role="form" action="?perfil=projeto" method="post">
+						<form class="form-horizontal" role="form" action="?perfil=projeto_pf" method="post">
 							<input type="submit" value="Avançar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPf ?>">
 						</form>
 					</div>
