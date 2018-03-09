ba9876e6c0048811089e08de5933911d8196691d
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-08 22:44:26 -0300

    Começo do envio do currículo

---
+++ perfil/projeto_3.php
@@ -0,0 +1,194 @@
+<?php
+$con = bancoMysqli();
+$idProjeto = $_SESSION['idProjeto'];
+
+if(isset($_POST['insere']))
+{
+	$resumoProjeto = $_POST['resumoProjeto'];
+	$curriculo = $_POST['curriculo'];
+
+	$sql_insere = "UPDATE projeto SET
+		resumoProjeto = '$resumoProjeto',
+		curriculo = '$curriculo'
+		WHERE idProjeto = '$idProjeto'";
+	if(mysqli_query($con,$sql_insere))
+	{
+		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+	}
+	else
+	{
+		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}
+}
+
+if(isset($_POST["enviar"]))
+{
+	$sql_arquivos = "SELECT * FROM upload_lista_documento WHERE idTipoPessoa = '$tipoPessoa' AND id = '$idCampo'";
+	$query_arquivos = mysqli_query($con,$sql_arquivos);
+	while($arq = mysqli_fetch_array($query_arquivos))
+	{
+		$y = $arq['id'];
+		$x = $arq['sigla'];
+		$nome_arquivo = $_FILES['arquivo']['name'][$x];
+		$f_size = $_FILES['arquivo']['size'][$x];
+
+		//Extensões permitidas
+		$ext = array("PDF","pdf");
+
+		if($f_size > 2097152) // 2MB em bytes
+		{
+			$mensagem = "Erro! Tamanho de arquivo excedido! Tamanho máximo permitido: 02 MB.";
+		}
+		else
+		{
+			if($nome_arquivo != "")
+			{
+				$nome_temporario = $_FILES['arquivo']['tmp_name'][$x];
+				$new_name = date("YmdHis")."_".semAcento($nome_arquivo); //Definindo um novo nome para o arquivo
+				$hoje = date("Y-m-d H:i:s");
+				$dir = '../uploadsdocs/'; //Diretório para uploads
+
+				$allowedExts = array(".pdf", ".PDF"); //Extensões permitidas
+
+				$ext = strtolower(substr($nome_arquivo,-4));
+
+				if(in_array($ext, $allowedExts)) //Pergunta se a extensão do arquivo, está presente no array das extensões permitidas
+				{
+					if(move_uploaded_file($nome_temporario, $dir.$new_name))
+					{
+						$sql_insere_arquivo = "INSERT INTO `upload_arquivo` (`idTipoPessoa`, `idPessoa`, `idUploadListaDocumento`, `arquivo`, `dataEnvio`, `publicado`) VALUES ('$tipoPessoa', '$idPessoaFisica', '$idCampo', '$new_name', '$hoje', '1'); ";
+						$query = mysqli_query($con,$sql_insere_arquivo);
+
+						if($query)
+						{
+							$mensagem = "Arquivo recebido com sucesso!";
+						}
+						else
+						{
+							$mensagem = "Erro ao gravar no banco!";
+						}
+					}
+					else
+					{
+						$mensagem = "Erro no upload. Tete novamente!";
+					}
+				}
+				else
+				{
+					$mensagem = "Erro no upload! Anexar documentos somente no formato PDF.";
+				}
+			}
+		}
+	}
+}
+
+if(isset($_POST['apagar']))
+{
+	$idArquivo = $_POST['apagar'];
+	$sql_apagar_arquivo = "UPDATE upload_arquivo SET publicado = 0 WHERE id = '$idArquivo'";
+	if(mysqli_query($con,$sql_apagar_arquivo))
+	{
+		$mensagem =	"Arquivo apagado com sucesso!";
+		gravarLog($sql_apagar_arquivo);
+	}
+	else
+	{
+		$mensagem = "Erro ao apagar o arquivo. Tente novamente!";
+	}
+}
+
+$projeto = recuperaDados("projeto","idProjeto",$idProjeto);
+?>
+<section id="list_items" class="home-section bg-white">
+    <div class="container">
+    	<?php
+    	if($_SESSION['tipoPessoa'] == 1)
+		{
+			$idPf= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pf.php';
+			$pf = recuperaDados("pessoa_fisica","idPf",$idPf);
+			$cooperado = $pf['cooperado'];
+		}
+		else
+		{
+			$idPj= $_SESSION['idUser'];
+			include '../perfil/includes/menu_interno_pj.php';
+		}
+    	?>
+		<div class="form-group">
+			<h4>Cadastro de Projeto</h4>
+			<p><strong><?php if(isset($mensagem)){echo $mensagem;} ?></strong></p>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<form method="POST" action="?perfil=projeto_3" class="form-horizontal" role="form">
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<label>Resumo do projeto*</label>
+							<textarea name="resumoProjeto" class="form-control" rows="5" maxlength="500"><?php echo $projeto['resumoProjeto'] ?></textarea>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<label>Currículo do proponente *</label>
+							<textarea name="curriculo" class="form-control" rows="10" maxlength="5000"><?php echo $projeto['curriculo'] ?></textarea>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<input type="submit" name="insere" class="btn btn-theme btn-lg btn-block" value="Gravar">
+						</div>
+					</div>
+				</form>
+
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8"><hr/></div>
+				</div>
+
+				<!-- Exibir arquivos -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class="table-responsive list_info"><h6>Arquivo(s) Anexado(s)</h6>
+							<?php //listaArquivoCamposMultiplos($idPessoaFisica,$tipoPessoa,$idCampo,"endereco_pf",3); ?>
+						</div>
+					</div>
+				</div>
+
+				<!-- Upload de arquivo -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class = "center">
+						<form method="POST" action="?perfil=endereco_pf" enctype="multipart/form-data">
+							<table>
+								<tr>
+									<td width="50%"><td>
+								</tr>
+								<?php
+									$sql_arquivos = "SELECT * FROM upload_lista_documento WHERE idTipoPessoa = '$tipoPessoa' AND id = '$idCampo'";
+									$query_arquivos = mysqli_query($con,$sql_arquivos);
+									while($arq = mysqli_fetch_array($query_arquivos))
+									{
+								?>
+										<tr>
+											<td><label><?php echo $arq['documento']?></label></td><td><input type='file' name='arquivo[<?php echo $arq['sigla']; ?>]'></td>
+										</tr>
+								<?php
+									}
+								?>
+							</table><br>
+							<input type="hidden" name="idPessoa" value="<?php echo $idPessoaFisica; ?>"  />
+							<input type="hidden" name="tipoPessoa" value="<?php echo $tipoPessoa; ?>"  />
+							<input type="submit" name="enviar" class="btn btn-theme btn-lg btn-block" value='Enviar'>
+						</form>
+						</div>
+					</div>
+				</div>
+				<!-- Fim Upload de arquivo -->
+
+			</div>
+		</div>
+	</div>
+</section>
