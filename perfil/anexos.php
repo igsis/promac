2faf38569e30a49e83de9e3b2dba299440f91393
José Campillo <jose.campillo@hotmail.com>
2018-03-12 17:46:53 -0300

    Alteração de título

--- perfil/anexos.php
+++ perfil/anexos.php
@@ -91,7 +91,7 @@ $pf = recuperaDados("pessoa_fisica","idPf",$idPf);
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_interno_pf.php'; ?>
 		<div class="form-group">
-			<h4>Arquivos Pessoais</h4>
+			<h4>Anexos</h4>
 			<h5><?php if(isset($mensagem)){echo $mensagem;};?></h5>
 		</div>
 		<div class="row">
