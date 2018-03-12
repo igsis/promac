d42f73482614011955d63588611f545c795ebebc
José Campillo <jose.campillo@hotmail.com>
2018-03-12 17:06:35 -0300

    Retirada de debug

--- funcoes/funcoesGerais.php
+++ funcoes/funcoesGerais.php
@@ -561,9 +561,6 @@ function verificaArquivosExistentes($idPessoa,$idDocumento)
 
 function listaArquivosPessoa($idPessoa,$tipoPessoa,$pagina)
 {
-	echo $idPessoa . "\n";
-	echo $tipoPessoa . "\n";
-	echo $pagina . "\n";
 	$con = bancoMysqli();
 	$sql = "SELECT *
 			FROM lista_documento as list
