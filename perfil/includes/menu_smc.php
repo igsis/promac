6bab4213b14e23f4a37d2bf6c1a48fc03b5c3e50
LoreleiGab <loreleigab@hotmail.com>
2018-03-14 12:20:54 -0300

    Menu da SMC

--- perfil/includes/menu_smc.php
+++ perfil/includes/menu_smc.php
@@ -1,36 +1,29 @@
 <?php
 //geram o insert pro framework da igsis
 $pasta = "?perfil=";
- ?>
+?>
 
 <div class="menu-area">
 	<div id="dl-menu" class="dl-menuwrapper">
 		<button class="dl-trigger">Open Menu</button>
 		<ul class="dl-menu">
-			<li><a href="#">Liberação de acesso</a>
+			<li><a href="<?php echo $pasta ?>smc_acesso_administrativo">Acesso administrativo</a></li>
+			<li><a href="#">Liberação de envio de projeto</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_acesso_pf">Pessoa Física</a></li>
-					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_acesso_pj">Pessoa Jurídica</a></li>
+					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_projeto_pf">Pessoa Física</a></li>
+					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_projeto_pj">Pessoa Jurídica</a></li>
 				</ul>
 			</li>
-			<li><a href="<?php echo $pasta ?>frm_busca_dataenvio">Filtro por Data de Envio</a></li>
-			<li><a href="#">Especiais</a>
+			<li><a href="#">Pesquisas</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>frm_lista_pedidocontratacao_pf&enviados=1">Formação</a></li>
-					<li><a href="<?php echo $pasta ?>frm_lista_pedidocontratacao_emia_pf&enviados=1">Emia</a></li>
-					<li><a href="#">Virada</a>
-						<ul class="dl-submenu">
-							<li><a href="<?php echo $pasta ?>frm_lista_projeto&atribuido=0">Sem nº Processo</a>
-							<li><a href="<?php echo $pasta ?>frm_lista_projeto&atribuido=1">Com nº Processo</a>
-							<li><a href="<?php echo $pasta ?>frm_lista_projeto&atribuido=3">Geral</a>
-						</ul>
-					</li>
+					<li><a href="<?php echo $pasta ?>smc_pesquisa_geral">Geral</a></li>
+					<li><a href="<?php echo $pasta ?>smc_pesquisa_prazos">Prazos</a></li>
 				</ul>
 			</li>
 			<li style="color:white;">-------------------------</li>
-			<li><a href="index.php?secao=perfil">Carregar módulos</a></li>
-			<li><a href="http://smcsistemas.prefeitura.sp.gov.br/igsis/manual/index.php/modulo-contratos/">Ajuda</a></li>
-			<li><a href="../index.php">Sair</a></li>
+			<li><a href="index.php?secao=perfil">Início</a></li>
+			<li><a href="../manual" target="_blank">Ajuda</a></li>
+			<li><a href="../include/logoff.php">Sair</a></li>
 		</ul>
 	</div>
 </div>
