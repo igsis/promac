7294ea697aa3efefd7f6fe5a674a2133c98ba370
José Campillo <jose.campillo@hotmail.com>
2018-03-15 17:11:52 -0300

    Telas de liberação de PF e PJ para SMC

---
+++ perfil/smc_visualiza_perfil_pf.php
@@ -0,0 +1,63 @@
+<?php
+$con = bancoMysqli();
+$tipoPessoa = '1';
+$projeto = $_SESSION['idProjeto'];
+$idPf = isset($_POST['liberado']) ? $_POST['liberado'] : null;
+$pf = recuperaDados("pessoa_fisica","idPf",$idPf);
+
+
+
+?>
+
+<section id="list_items" class="home-section bg-white">
+	<div class="container"><?php include 'includes/menu_interno_pf.php'; ?>
+		<div class="form-group">
+
+			<h4>Resumo do Usuário</h4>
+			<div class="alert alert-warning">
+				<strong>Atenção!</strong> Confirme atentamente se os dados abaixo estão corretos!
+			</div>
+		</div>
+		 <div class = "page-header">
+		 	<h5>Informações pessoais</h5>
+		 	<br>
+		 </div>
+
+		 <div class="well">
+			<p align="justify"><strong>Referência:</strong> <?php echo $pf['idPf']; ?></p>
+			<p align="justify"><strong>Nome:</strong> <?php echo isset($pf['nome']) ? $pf['nome'] : null; ?></p>
+			<p align="justify"><strong>CPF:</strong> <?php echo isset($pf['cpf']) ? $pf['cpf'] : null; ?><p>
+			<p align="justify"><strong>RG:</strong> <?php echo isset($pf['rg']) ? $pf['rg'] : null; ?><p>
+		</div>
+
+		<div class = "page-header">
+		 	<h5>Endereço e contato</h5>
+		 	<br>
+		 </div>
+
+		  <div class="well">
+			<p align="justify"><strong>Logradouro:</strong> <?php echo isset($pf['logradouro']) ? $pf['logradouro'] : null; ?></p>
+			<p align="justify"><strong>Número:</strong> <?php echo isset($pf['numero']) ? $pf['numero'] : null; ?></p>
+			<p align="justify"><strong>Complemento:</strong> <?php echo isset($pf['complemento']) ? $pf['complemento'] : null; ?></p>
+			<p align="justify"><strong>CEP:</strong> <?php echo isset($pf['cep']) ? $pf['cep'] : null; ?></p>
+			<p align="justify"><strong>Bairro:</strong> <?php echo isset($pf['bairro']) ? $pf['bairro'] : null; ?></p>
+			<p align="justify"><strong>Cidade:</strong> <?php echo isset($pf['cidade']) ? $pf['cidade'] : null; ?></p>
+			<p align="justify"><strong>Estado:</strong> <?php echo isset($pf['estado']) ? $pf['estado'] : null; ?></p>
+			<p align="justify"><strong>Telefone:</strong> <?php echo isset($pf['telefone']) ? $pf['telefone'] : null; ?></p>
+			<p align="justify"><strong>Celular:</strong> <?php echo isset($pf['celular']) ? $pf['celular'] : null; ?></p>
+			<p align="justify"><strong>Email:</strong> <?php echo isset($pf['email']) ? $pf['email'] : null; ?></p>
+		 </div>
+		 <div class="table-responsive list_info"><h6>Arquivo(s) de Pessoa Física</h6>
+		<?php listaArquivosPessoa($idPf,'1',"visualiza_perfil_pf"); ?>
+		</div>
+	</div>
+
+<!-- Botão para Prosseguir -->
+	<div class="form-group">
+		<div class="col-md-offset-5 col-md-2">
+			<form class="form-horizontal" role="form" action="?perfil=lista_liberacao" method="post">
+				<input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block">
+			</form>
+		</div>
+	</div>
+</section>
---
+++ perfil/smc_visualiza_perfil_pj.php
@@ -0,0 +1,61 @@
+<?php
+$con = bancoMysqli();
+$tipoPessoa = '2';
+
+$idPj = $_POST['liberado'];
+$pj = recuperaDados("pessoa_juridica","idPj",$idPj);
+
+?>
+
+<section id="list_items" class="home-section bg-white">
+	<div class="container"><?php include 'includes/menu_interno_pf.php'; ?>
+		<div class="form-group">
+
+			<h4>Resumo do Usuário</h4>
+			<div class="alert alert-warning">
+				<strong>Atenção!</strong> Confirme atentamente se os dados abaixo estão corretos!
+			</div>
+		</div>
+		 <div class = "page-header">
+		 	<h5>Informações pessoais</h5>
+		 	<br>
+		 </div>
+
+		 <div class="well">
+			<p align="justify"><strong>Referência:</strong> <?php echo $pj['idPj']; ?></p>
+			<p align="justify"><strong>Razão Social:</strong> <?php echo isset($pj['razaoSocial']) ? $pj['razaoSocial'] : null; ?></p>
+			<p align="justify"><strong>CNPJ:</strong> <?php echo isset($pj['cnpj']) ? $pj['cnpj'] : null; ?><p>
+			<p align="justify"><strong>CCM:</strong> <?php echo isset($pj['ccm']) ? $pj['ccm'] : null; ?><p>
+		</div>
+
+		<div class = "page-header">
+		 	<h5>Endereço e contato</h5>
+		 	<br>
+		 </div>
+
+		  <div class="well">
+			<p align="justify"><strong>Logradouro:</strong> <?php echo isset($pj['logradouro']) ? $pj['logradouro'] : null; ?></p>
+			<p align="justify"><strong>Número:</strong> <?php echo isset($pj['numero']) ? $pj['numero'] : null; ?></p>
+			<p align="justify"><strong>Complemento:</strong> <?php echo isset($pj['complemento']) ? $pj['complemento'] : null; ?></p>
+			<p align="justify"><strong>CEP:</strong> <?php echo isset($pj['cep']) ? $pj['cep'] : null; ?></p>
+			<p align="justify"><strong>Bairro:</strong> <?php echo isset($pj['bairro']) ? $pj['bairro'] : null; ?></p>
+			<p align="justify"><strong>Cidade:</strong> <?php echo isset($pj['cidade']) ? $pj['cidade'] : null; ?></p>
+			<p align="justify"><strong>Estado:</strong> <?php echo isset($pj['estado']) ? $pj['estado'] : null; ?></p>
+			<p align="justify"><strong>Telefone:</strong> <?php echo isset($pj['telefone']) ? $pj['telefone'] : null; ?></p>
+			<p align="justify"><strong>Celular:</strong> <?php echo isset($pj['celular']) ? $pj['celular'] : null; ?></p>
+			<p align="justify"><strong>Email:</strong> <?php echo isset($pj['email']) ? $pj['email'] : null; ?></p>
+		 </div>
+		 <div class="table-responsive list_info"><h6>Arquivo(s) de Pessoa Física</h6>
+		<?php listaArquivosPessoa($idPj,'2',"visualiza_perfil_pj"); ?>
+		</div>
+	</div>
+
+<!-- Botão para Prosseguir -->
+	<div class="form-group">
+		<div class="col-md-offset-5 col-md-2">
+			<form class="form-horizontal" role="form" action="?perfil=lista_liberacao" method="post">
+				<input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block">
+			</form>
+		</div>
+	</div>
+</section>
