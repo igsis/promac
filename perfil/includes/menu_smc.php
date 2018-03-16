37b68d51ab9cfef3142723f7ec3d2c7f0c3ea5e8
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-16 20:12:08 -0300

    Ajustes no menu da SMC

--- perfil/includes/menu_smc.php
+++ perfil/includes/menu_smc.php
@@ -1,17 +1,15 @@
 <?php
-//geram o insert pro framework da igsis
 $pasta = "?perfil=";
 ?>
-
 <div class="menu-area">
 	<div id="dl-menu" class="dl-menuwrapper">
 		<button class="dl-trigger">Open Menu</button>
 		<ul class="dl-menu">
-			<li><a href="<?php echo $pasta ?>smc_acesso_administrativo">Acesso administrativo</a></li>
+			<li><a href="<?php echo $pasta ?>smc_index">Home</a></li>
 			<li><a href="#">Liberação de envio de projeto</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_projeto_pf">Pessoa Física</a></li>
-					<li><a href="<?php echo $pasta ?>smc_lista_liberacao_projeto_pj">Pessoa Jurídica</a></li>
+					<li><a href="<?php echo $pasta ?>smc_lista_liberacao&tipo=1">Pessoa Física</a></li>
+					<li><a href="<?php echo $pasta ?>smc_lista_liberacao&tipo=2">Pessoa Jurídica</a></li>
 				</ul>
 			</li>
 			<li><a href="#">Pesquisas</a>
@@ -20,12 +18,7 @@ $pasta = "?perfil=";
 					<li><a href="<?php echo $pasta ?>smc_pesquisa_prazos">Prazos</a></li>
 				</ul>
 			</li>
-			<li><a href="#">Nivel de acesso</a>
-				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>smc_pesquisa_nome">Tipo de Acesso</a></li>
-					
-				</ul>
-			</li>
+			<li><a href="<?php echo $pasta ?>smc_pesquisa_nome">Nivel de acesso</a></li>
 			<li style="color:white;">-------------------------</li>
 			<li><a href="index.php?secao=perfil">Início</a></li>
 			<li><a href="../manual" target="_blank">Ajuda</a></li>
