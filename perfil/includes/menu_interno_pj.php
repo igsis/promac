afafef29f65f86851191aa499d466d2030b34162
LoreleiGab <loreleigab@hotmail.com>
2018-03-21 13:30:13 -0300

    Ajustes no menu pj

--- perfil/includes/menu_interno_pj.php
+++ perfil/includes/menu_interno_pj.php
@@ -1,11 +1,16 @@
 <div class="row">
 	<div class="form-group">
-		<div class="col-md-offset-2 col-md-8">
-			<strong>
-			| <a href="?secao=perfil">Início</a>
-			| <a href="../manual" target="_blank">Ajuda</a>
-			| <a href="../include/logoff.php">Sair</a> |<br/>
-		</strong>
+		<div class="col-md-offset-7 col-md-4">
+			<ul class="list-group">
+				<li class="list-group-item list-group-item-success">
+					<strong>
+					| <a href="?secao=perfil">Início</a>
+					| <a href="?perfil=minha_conta">Trocar minha senha</a>
+					| <a href="../manual" target="_blank">Ajuda</a>
+					| <a href="../include/logoff.php">Sair</a> |<br/>
+					</strong>
+				</li>
+			</ul>
 		</div>
 	</div>
 </div>
