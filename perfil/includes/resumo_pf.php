c11b09990d3e3804a2f4cfbfe76d2269aa2f4631
LoreleiGab <loreleigab@hotmail.com>
2018-03-21 15:27:01 -0300

    Mudança no texto do resumo_pf

--- perfil/includes/resumo_pf.php
+++ perfil/includes/resumo_pf.php
@@ -21,16 +21,22 @@
 	?>
 </div>
 <div class="page-header">
- 	<h5>Informações Pessoais</h5>
+ 	<h5>Dados do Proponente</h5>
 </div>
 <div class="well">
-	<p align="justify"><strong>Referência:</strong> <?php echo $pf['idPf']; ?></p>
 	<p align="justify"><strong>Nome:</strong> <?php echo $pf['nome']; ?></p>
 	<p align="justify"><strong>CPF:</strong> <?php echo $pf['cpf']; ?><p>
 	<p align="justify"><strong>RG ou RNE: </strong><?php echo $pf['rg']; ?><p>
 	<p align="justify"><strong>Email:</strong> <?php echo $pf['email']; ?><p>
 	<p align="justify"><strong>Telefone:</strong> <?php echo $pf['telefone']; ?><p>
 	<p align="justify"><strong>Celular:</strong> <?php echo $pf['celular']; ?></p>
+	<p align="justify"><strong>CEP:</strong> <?php echo $pf['cep']; ?></p>
+	<p align="justify"><strong>Logradouro:</strong> <?php echo $pf['logradouro']; ?></p>
+	<p align="justify"><strong>Número:</strong> <?php echo $pf['numero']; ?></p>
+	<p align="justify"><strong>Complemento:</strong> <?php echo $pf['complemento']; ?></p>
+	<p align="justify"><strong>Bairro:</strong> <?php echo $pf['bairro']; ?></p>
+	<p align="justify"><strong>Cidade:</strong> <?php echo $pf['cidade']; ?></p>
+	<p align="justify"><strong>Estado:</strong> <?php echo $pf['estado']; ?></p>
 	<p align="justify"><strong>É Cooperado?:</strong>
 		<?php
 			if ($pf['cooperado'] == 0)
@@ -42,20 +48,6 @@
 				echo "Sim";
 			}
 		?></p>
-</div>
-<div class="table-responsive list_inf">
-	<div class = "page-header">
-		<h5>Endereço:</h5>
-	</div>
-	<div class="well">
-		<p align="justify"><strong>CEP:</strong> <?php echo $pf['cep']; ?></p>
-		<p align="justify"><strong>Logradouro:</strong> <?php echo $pf['logradouro']; ?></p>
-		<p align="justify"><strong>Número:</strong> <?php echo $pf['numero']; ?></p>
-		<p align="justify"><strong>Complemento:</strong> <?php echo $pf['complemento']; ?></p>
-		<p align="justify"><strong>Bairro:</strong> <?php echo $pf['bairro']; ?></p>
-		<p align="justify"><strong>Cidade:</strong> <?php echo $pf['cidade']; ?></p>
-		<p align="justify"><strong>Estado:</strong> <?php echo $pf['estado']; ?></p>
-	</div>
 </div>
 
 <div class="table-responsive list_info"><h6>Arquivo(s) de Pessoa Física(s)</h6>
