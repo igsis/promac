a3180c63b2d7cb6e35f6003819fdc996f998b6f1
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-21 22:06:40 -0300

    Mudanças no menu da SMC para telas de pesquisa

--- perfil/includes/menu_smc.php
+++ perfil/includes/menu_smc.php
@@ -6,22 +6,19 @@ $pasta = "?perfil=";
 		<button class="dl-trigger">Open Menu</button>
 		<ul class="dl-menu">
 			<li><a href="<?php echo $pasta ?>smc_index">Home</a></li>
-			<li><a href="#">Inscrição de Projeto</a>
+			<li><a href="<?php echo $pasta ?>smc_pesquisa_nome">Nivel de acesso</a></li>
+			<li><a href="#">Pesquisas</a>
 				<ul class="dl-submenu">
 					<li><a href="<?php echo $pasta ?>smc_pesquisa_pf">Pessoa Física</a></li>
 					<li><a href="<?php echo $pasta ?>smc_pesquisa_pj">Pessoa Jurídica</a></li>
-				</ul>
-			</li>
-			<li><a href="#">Pesquisas</a>
-				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>smc_pesquisa_geral">Geral</a></li>
 					<li><a href="<?php echo $pasta ?>smc_pesquisa_prazos">Prazos</a></li>
+					<li><a href="<?php echo $pasta ?>smc_pesquisa_geral">Projetos</a></li>
 				</ul>
 			</li>
-			<li><a href="<?php echo $pasta ?>smc_pesquisa_nome">Nivel de acesso</a></li>
 			<li><a href="?perfil=informacoes_iniciais_pf">Módulo Proponente</a></li>
 			<li style="color:white;">-------------------------</li>
 			<li><a href="index.php?secao=perfil">Início</a></li>
+			<li><a href="<?php echo $pasta ?>minha_conta">Minha conta</a></li>
 			<li><a href="../manual" target="_blank">Ajuda</a></li>
 			<li><a href="../include/logoff.php">Sair</a></li>
 		</ul>
