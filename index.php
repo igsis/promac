2dae89d45d94859acec07c3f7ecf3e57cb52f60d
LoreleiGab <loreleigab@hotmail.com>
2018-03-22 15:35:59 -0300

    Implantação do logo e rodapé na tela inicial do sistema

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>SMC / Pro-Mac - Programa Municipal de Apoio a Projetos Culturais</title>
@@ -19,7 +19,8 @@
 				<div class="row">
 					<div class="col-md-offset-1 col-md-10">
 						<h4>Pro-Mac - Programa Municipal de Apoio a Projetos Culturais</h4>
-						<p align="justify">Inspirado no Programa de Ação Cultural (ProAC) do Governo de São Paulo, o Pro-Mac permite que o patrocinador tenha dedução de 100% do valor investido, disponibilizando mais recursos para artistas e produtores culturais, que antes recebiam no máximo 70% do valor total do patrocínio. Com o ProMAC, áreas como design, cultura digital, séries de TV, espaços culturais independentes, circo, cinema, dança, fotografia, teatro – dentre outras – que não estavam previstas na Lei Mendonça- agora podem receber patrocínio.</p>
+						<img src="visual/images/logo_promac_colorido.png" align="left" hspace="20" />
+						<p align="justify">Inspirado no Programa de Ação Cultural (ProAC) do Governo de São Paulo, o Pro-Mac permite que o patrocinador tenha dedução de 100% do valor investido, disponibilizando mais recursos para artistas e produtores culturais, que antes recebiam no máximo 70% do valor total do patrocínio. Com o Pro-Mac, áreas como design, cultura digital, séries de TV, espaços culturais independentes, circo, cinema, dança, fotografia, teatro – dentre outras – que não estavam previstas na Lei Mendonça  – agora podem receber patrocínio.</p>
 
 						<p align="justify">Ao estabelecer regras simples e critérios mais objetivos para a obtenção do incentivo, a nova Lei se torna acessível a pequenos produtores e a artistas independentes, que antes dependiam de empresas e produtoras culturais para adequarem seus projetos à burocracia e às exigências da Lei Mendonça. Outro destaque é que o Pro-Mac também prevê que a contrapartida ao patrocínio, que antes era determinada por lei (Ex: o artista deverá produzir e distribuir 1000 CDs ou realizar 10 shows gratuitos) seja formulada pelo proponente do projeto. Isso significa que o autor da proposta é quem vai indicar a melhor forma de levar ao público o seu projeto cultural, o que também será um critério de avaliação.</p>
 
@@ -30,18 +31,23 @@
 						<div class="form-group">
 							<div class="col-md-3">
 								<form method="POST" action="login_pf.php" class="form-horizontal" role="form">
-									<button type="submit" class="btn btn-theme btn-lg btn-block">Pessoa Física</button>
+									<button type="submit" class="btn btn-theme btn-md btn-block">Pessoa Física</button>
 								</form>
 							</div>
-							<div class="col-md-offset-1 col-md-3">
+							<div class="col-md-3">
 								<form method="POST" action="login_pj.php" class="form-horizontal" role="form">
-									<button type="submit" class="btn btn-theme btn-lg btn-block">Pessoa Jurídica</button>
+									<button type="submit" class="btn btn-theme btn-md btn-block">Pessoa Jurídica</button>
 								</form>
 							</div>
-							<div class="col-md-offset-1 col-md-3">
+							<div class="col-md-3">
+								<form method="POST" action="#" class="form-horizontal" role="form">
+									<button type="submit" class="btn btn-theme btn-md btn-block">Incentivador</button>
+								</form>
+							</div>
+							<div class="col-md-3">
 								<form method="POST" action="./visual/index.php" class="form-horizontal" role="form">
 									<input type="hidden" name="consulta" value="1">
-									<button type="submit" class="btn btn-theme btn-lg btn-block">Consulta Pública</button>
+									<button type="submit" class="btn btn-theme btn-md btn-block">Consulta Pública</button>
 								</form>
 							</div>
 						</div>
@@ -53,7 +59,9 @@
 			<div class="container">
 				<table width="100%">
 					<tr>
-						<td align="center"><font color="#ccc">2018 @ ProMAC - Programa Municipal de Apoio a Projetos Culturais<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
+						<td width="20%"><img src="visual/images/logo_promac_branco.png" align="left"/></td>
+				<td align="center"><font color="#ccc">2018 @ Pro-Mac - Programa Municipal de Apoio a Projetos Culturais<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
+				<td width="20%"><img src="visual/images/logo_cultura_q.png" align="right"/></td>
 					</tr>
 				</table>
 			</div>
