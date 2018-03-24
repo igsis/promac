dc8e799c706c5b183ce9fb00b97adaf9a2b563a8
Lorelei Gabriele <loreleigab@hotmail.com>
2018-03-24 10:40:29 -0300

    Lista de projetos enviados na index da smc

--- perfil/smc_index.php
+++ perfil/smc_index.php
@@ -140,6 +140,74 @@ if(isset($_POST['liberacaoPJ']))
 			</div>
 		</div>
 		<div class="form-group">
+			<h5>Lista de projetos enviados.</h5>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<div class="table-responsive list_info">
+				<?php
+					$sql = $sql = "SELECT * FROM projeto WHERE publicado = 1 AND idStatus = 2 LIMIT 0,10";
+					$query = mysqli_query($con,$sql);
+					$num = mysqli_num_rows($query);
+					if($num > 0)
+					{
+						echo "
+							<table class='table table-condensed'>
+								<thead>
+									<tr class='list_menu'>
+										<td>Protocolo (nº ISP)</td>
+										<td>Nome do Projeto</td>
+										<td>Proponente</td>
+										<td>Documento</td>
+										<td>Área de Atuação</td>
+										<td width='10%'></td>
+									</tr>
+								</thead>
+								<tbody>";
+
+								while($campo = mysqli_fetch_array($query))
+								{
+									$area = recuperaDados("area_atuacao","idArea",$campo['idAreaAtuacao']);
+									$status = recuperaDados("status","idStatus",$campo['idStatus']);
+									$pf = recuperaDados("pessoa_fisica","idPf",$campo['idPf']);
+									$pj = recuperaDados("pessoa_juridica","idPj",$campo['idPj']);
+
+									echo "<tr>";
+									echo "<td class='list_description'>".$campo['protocolo']."</td>";
+									echo "<td class='list_description'>".$campo['nomeProjeto']."</td>";
+									if($campo['tipoPessoa'] == 1)
+									{
+										echo "<td class='list_description'>".$pf['nome']."</td>";
+										echo "<td class='list_description'>".$pf['cpf']."</td>";
+									}
+									if($campo['tipoPessoa'] == 2)
+									{
+										echo "<td class='list_description'>".$pf['razaoSocial']."</td>";
+										echo "<td class='list_description'>".$pf['cnpj']."</td>";
+									}
+									echo "<td class='list_description'>".$area['areaAtuacao']."</td>";
+									echo "
+										<td class='list_description'>
+											<form method='POST' action='?perfil=comissao_detalhes_projeto'>
+												<input type='hidden' name='idProjeto' value='".$campo['idProjeto']."' />
+												<input type ='submit' class='btn btn-theme btn-block' value='Visualizar'>
+											</form>
+										</td>";
+									}
+									echo "</tr>";
+						echo "
+							</tbody>
+							</table>";
+						}
+						else
+						{
+							echo "Nada consta.";
+						}
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="form-group">
 			<h5>Projetos com data de captação com tempo menor que 30 dias.</h5>
 		</div>
 		<div class="row">
