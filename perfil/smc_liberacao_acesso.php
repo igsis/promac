0a7892c3d03e758cc165615bc3363477673b7ec6
Isaias Mendes <isaiaszmendes@gmail.com>
2018-03-26 10:11:29 -0300

    Correções

--- perfil/smc_liberacao_acesso.php
+++ perfil/smc_liberacao_acesso.php
@@ -5,8 +5,6 @@ $conn = bancoPDO();
 
 $nome = isset($_POST['nome'])?$_POST['nome']:null;
 $cpf  = isset($_POST['cpf'])?$_POST['cpf']:null;
-$razaoSocial = isset($_POST['razaoSocial'])? $_POST['razaoSocial']:null;
-$cnpj = isset($_POST['cnpj'])?$_POST['cnpj']:null;
 
 
 if($nome != '' || $cpf != '')
@@ -40,12 +38,23 @@ if($nome != '' || $cpf != '')
 
 }
 if(isset($_POST['pesquisarPf'])){
-	$sql = "SELECT * FROM pessoa_fisica 
-			WHERE $filtro_cpf $filtro $filtro_nome";
+	if($nome == true || $cpf == true ){
 
-	$stmt = $conn->prepare($sql);
-	$stmt->execute();
-	$result = $stmt->fetchAll();
+		$sql = "SELECT * FROM pessoa_fisica 
+				WHERE $filtro_cpf $filtro $filtro_nome";
+
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+	}
+	if($nome == '' && $cpf == '' ){
+
+		$sql = "SELECT * FROM pessoa_fisica";
+
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+	}
 
 }
 if (isset($_POST['alterarPf'])) {
@@ -57,7 +66,9 @@ if (isset($_POST['alterarPf'])) {
  	$stmt = $conn->prepare($sql_atualizar);
  	$stmt->bindParam(':idPf', $id);
  	$stmt->bindParam(':idNivelAcesso', $idNivelAcesso);
- 	$stmt->execute();
+ 	if ($stmt->execute()) {
+ 		$mensagem = 'Atualizado com sucesso!';
+ 	}
 
  	$sql = "SELECT * FROM pessoa_fisica 
 		WHERE idPf = $id ";
@@ -66,69 +77,13 @@ if (isset($_POST['alterarPf'])) {
 	$result = $stmt->fetchAll();
 }
 
-if($razaoSocial != '' || $cnpj != '')
-{
-	if($razaoSocial != '')
-	{
-		$filtro_razaoSocial = "razaoSocial LIKE '%$razaoSocial%'";
-	}
-	else
-	{
-		$filtro_razaoSocial = "";
-	}
-
-	if($cnpj != '')
-	{
-		$filtro_cnpj = "cnpj = '$cnpj'";
-	}
-	else
-	{
-		$filtro_cnpj = "";
-	}
-
-	if ($razaoSocial != '' && $cnpj != '')
-	{
-		$filtro = " AND ";
-	}
-	else
-	{
-		$filtro = '';
-	}
-}
-if(isset($_POST['pesquisarPj'])){
-	$sql = "SELECT * FROM pessoa_juridica
-			WHERE $filtro_cnpj $filtro $filtro_razaoSocial";
-
-	$stmt = $conn->prepare($sql);
-	$stmt->execute();
-	$result = $stmt->fetchAll();
-
-}
-if (isset($_POST['alterarPj'])) {
-	$id = $_POST['id'];
-	$idNivelAcesso = $_POST['idNivelAcesso'];
-	$sql_atualizar = "UPDATE pessoa_juridica SET
-		idNivelAcesso = :idNivelAcesso
- 		WHERE idPj = :idPj ";
- 	$stmt = $conn->prepare($sql_atualizar);
- 	$stmt->bindParam(':idPj', $id);
- 	$stmt->bindParam(':idNivelAcesso', $idNivelAcesso);
- 	$stmt->execute();
-
- 	$sql = "SELECT * FROM pessoa_juridica 
-		WHERE idPj = $id ";
-	$stmt = $conn->prepare($sql);
-	$stmt->execute();
-	$result = $stmt->fetchAll();
-}
-
-if($nome == true || $cpf == true || isset($_POST['alterarPf'])){
 ?>
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_smc.php'; ?>
 		<div class="form-group">
 			<h4>Resultado da Busca</h4><hr>
 			<h5><a href="?perfil=smc_pesquisa_nome">Fazer outra busca</a></h5>
+			<h6><?php echo isset($mensagem) ? $mensagem : ''; ?></h6>
 			<div class="col-md-offset-2 col-md-8">
 				<div class="table-responsive list_info">
 					<table class='table table-condensed table-hover text-center'>
@@ -143,36 +98,40 @@ if($nome == true || $cpf == true || isset($_POST['alterarPf'])){
 						</thead>
 						<tbody>
 							<?php
-							foreach ($result as $value)
-							{
-								if ($value['idNivelAcesso'] == 1) {
-									$acesso = 'Proponente';
-								}elseif ($value['idNivelAcesso'] == 2){
-									$acesso = 'SMC';
-								}elseif ($value['idNivelAcesso'] == 3) {
-									$acesso = 'Comissão';
-								}else {
-									$acesso = '';
+							if (isset($result)) {
+								
+							
+								foreach ($result as $value)
+								{
+									if ($value['idNivelAcesso'] == 1) {
+										$acesso = 'Proponente';
+									}elseif ($value['idNivelAcesso'] == 2){
+										$acesso = 'SMC';
+									}elseif ($value['idNivelAcesso'] == 3) {
+										$acesso = 'Comissão';
+									}else {
+										$acesso = '';
+									}
+									echo "<form method='POST' action=''>";
+									echo "<tr>";
+									echo "<td class='list_description'>".$value['idPf']."</td>";
+									echo "<td class='list_description'>".$value['nome']."</td>";
+									echo "<td class='list_description'>".$value['cpf']."</td>";
+									echo "<td class='list_description'>
+											<select class='form-control' name='idNivelAcesso'>
+												<option value='".$value['idNivelAcesso']."'>".$acesso."</option>
+												<option value='1'>Proponente</option>
+												<option value='2'>SMC</option>
+												<option value='3'>Comissão</option>									
+											</select>
+										</td>";
+									echo "<td class='list_description'>
+											<input type='hidden' name='id' value='".$value['idPf']."' />
+											<input type ='submit' name='alterarPf' class='btn btn-theme btn-block btn-sm' value='alterar'>						
+										</td>";				
+									echo "</tr>";
+									echo "</form>";
 								}
-								echo "<form method='POST' action=''>";
-								echo "<tr>";
-								echo "<td class='list_description'>".$value['idPf']."</td>";
-								echo "<td class='list_description'>".$value['nome']."</td>";
-								echo "<td class='list_description'>".$value['cpf']."</td>";
-								echo "<td class='list_description'>
-										<select class='form-control' name='idNivelAcesso'>
-											<option value='".$value['idNivelAcesso']."'>".$acesso."</option>
-											<option value='1'>Proponente</option>
-											<option value='2'>SMC</option>
-											<option value='3'>Comissão</option>									
-										</select>
-									</td>";
-								echo "<td class='list_description'>
-										<input type='hidden' name='id' value='".$value['idPf']."' />
-										<input type ='submit' name='alterarPf' class='btn btn-theme btn-block btn-sm' value='alterar'>						
-									</td>";				
-								echo "</tr>";
-								echo "</form>";
 							}
 							?>
 						</tbody>
@@ -182,68 +141,3 @@ if($nome == true || $cpf == true || isset($_POST['alterarPf'])){
 		</div>		
 	</div>
 </section>
-<?php 
-	}elseif ($razaoSocial == true || $cnpj == true || isset($_POST['alterarPj'])){
-	
-?>
-<section id="list_items" class="home-section bg-white">
-	<div class="container"><?php include 'includes/menu_smc.php'; ?>
-		<div class="form-group">
-			<h4>Resultado da Busca</h4><hr>
-			<h5><a href="?perfil=smc_pesquisa_nome">Fazer outra busca</a></h5>
-			<div class="col-md-offset-2 col-md-8">
-				<div class="table-responsive list_info">
-					<table class='table table-condensed table-hover text-center'>
-						<thead>
-							<tr class='list_menu'>
-								<td>ID</td>
-								<td>Razão Social</td>
-								<td>CNPJ</td>
-								<td>Tipo Acesso</td>
-								<td width='15%'></td>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							foreach ($result as $value)
-							{
-								if ($value['idNivelAcesso'] == 1) {
-									$acesso = 'Proponente';
-								}elseif ($value['idNivelAcesso'] == 2){
-									$acesso = 'SMC';
-								}elseif ($value['idNivelAcesso'] == 3) {
-									$acesso = 'Comissão';
-								}else {
-									$acesso = '';
-								}
-								echo "<form method='POST' action=''>";
-								echo "<tr>";
-								echo "<td class='list_description'>".$value['idPj']."</td>";
-								echo "<td class='list_description'>".$value['razaoSocial']."</td>";
-								echo "<td class='list_description'>".$value['cnpj']."</td>";
-								echo "<td class='list_description'>
-										<select class='form-control' name='idNivelAcesso'>
-											<option value='".$value['idNivelAcesso']."'>".$acesso."</option>
-											<option value='1'>Proponente</option>
-											<option value='2'>SMC</option>
-											<option value='3'>Comissão</option>								
-										</select>
-									</td>";
-								echo "<td class='list_description'>
-										<input type='hidden' name='id' value='".$value['idPj']."' />
-										<input type ='submit' name='alterarPj' class='btn btn-theme btn-block btn-sm' value='alterar'>						
-									</td>";				
-								echo "</tr>";
-								echo "</form>";
-							}
-							?>
-						</tbody>
-					</table>
-				</div>
-			</div>	
-		</div>		
-	</div>
-</section>
-<?php 
-	}
- ?>
--- visual/barras_smart_wizard/barra_incentivador_pf.php
+++ visual/barras_smart_wizard/barra_incentivador_pf.php
@@ -1,10 +1,13 @@
 <?php 
+# Barra Incentivador PF
 $con  = bancoMysqli();
 
 $urlIncentivadorPf = array(
 	// Rotas
 	0 => '/promac/visual/incentivador_index_pf.php',
-	1 => '/promac/visual/incentivador_index_pf.php?perfil=cadastro_incentivador_pf'
+	1 => '/promac/visual/incentivador_index_pf.php?perfil=cadastro_incentivador_pf',
+	2 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pf',
+	3 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_arquivos_pf'
 
 );
 
@@ -24,7 +27,7 @@ for ($i = 0; $i < count($urlIncentivadorPf); $i++) {
                 <li class="<?php echo isset($ligar1) ? $ligar1 : 'clickable'; ?>">
                     <a onclick="location.href='incentivador_index_pf.php'" href=""><br />Incentivador</a>
                 </li>
-                <li class="<?php echo isset($liga2) ? $liga2 : 'clickable'; ?>">
+                <li class="<?php echo isset($ligar2) ? $ligar2 : 'clickable'; ?>">
                     <a onclick="location.href=''" href=""><br />Documentos do Incentivador</a>
                 </li>
             </ul>
