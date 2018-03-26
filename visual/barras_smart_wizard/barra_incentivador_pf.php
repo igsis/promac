8dfdac80e00e63d8650a662fe168b32c92d0fb5a
Isaias Mendes <isaiaszmendes@gmail.com>
2018-03-26 11:20:16 -0300

    Correções

--- visual/barras_smart_wizard/barra_incentivador_pf.php
+++ visual/barras_smart_wizard/barra_incentivador_pf.php
@@ -7,7 +7,8 @@ $urlIncentivadorPf = array(
 	0 => '/promac/visual/incentivador_index_pf.php',
 	1 => '/promac/visual/incentivador_index_pf.php?perfil=cadastro_incentivador_pf',
 	2 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pf',
-	3 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_arquivos_pf'
+	3 => '/promac/visual/incentivador_index_pj.php?perfil=arquivos_incentivador_pf',
+	4 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_arquivos_pf'
 
 );
 
@@ -15,23 +16,30 @@ for ($i = 0; $i < count($urlIncentivadorPf); $i++) {
     if ($uri == $urlIncentivadorPf[$i]) {
         if ($i == 0|| $i == 1) {
             $ligar1 = 'active loading';
+        }elseif ($i == 2) {
+        	$ligar2 = 'active loading';
+        }elseif ($i == 3) {
+        	$ligar3 = 'active loading';
         }
 ?>
 
 	<!-- SmartWizard html -->
-        <div id="smartwizard">
-            <ul>
-                <li class="hidden">
-                    <a href=""><br /></a>
-                </li>
-                <li class="<?php echo isset($ligar1) ? $ligar1 : 'clickable'; ?>">
-                    <a onclick="location.href='incentivador_index_pf.php'" href=""><br />Incentivador</a>
-                </li>
-                <li class="<?php echo isset($ligar2) ? $ligar2 : 'clickable'; ?>">
-                    <a onclick="location.href=''" href=""><br />Documentos do Incentivador</a>
-                </li>
-            </ul>
-        </div>
+    <div id="smartwizard">
+        <ul>
+            <li class="hidden">
+                <a href=""><br /></a>
+            </li>
+            <li class="<?php echo isset($ligar1) ? $ligar1 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pf.php'" href=""><br />Cadastro Incentivador</a>
+            </li>
+            <li class="<?php echo isset($ligar2) ? $ligar2 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pf'" href=""><br />Incentivador</a>
+            </li>
+            <li class="<?php echo isset($ligar3) ? $ligar3 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pj.php?perfil=arquivos_incentivador_pf'" href=""><br />Documentos do Incentivador</a>
+            </li>
+        </ul>
+    </div>
 <?php  
 	}
 }
--- visual/barras_smart_wizard/barra_incentivador_pj.php
+++ visual/barras_smart_wizard/barra_incentivador_pj.php
@@ -6,38 +6,50 @@ $urlIncentivadorPj = array(
 	// Rotas
 	0 => '/promac/visual/incentivador_index_pj.php',
 	1 => '/promac/visual/incentivador_index_pj.php?perfil=cadastro_incentivador_pj',
-	2 => '/promac/visual/incentivador_index_pj.php?perfil=arquivos_incentivador_pj',
-	3 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pj',
-	4 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj',
-	5 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj_resultado_busca',
-	6 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj_cadastro'
+	2 => '/promac/visual/incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pj',
+	3 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj',
+	4 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj_resultado_busca',
+	5 => '/promac/visual/incentivador_index_pj.php?perfil=incentivador_representante_pj_cadastro',
+	6 => '/promac/visual/incentivador_index_pj.php?perfil=representante_pj',
+	7 => '/promac/visual/incentivador_index_pj.php?perfil=representante_pj_cadastro',
+	8 => '/promac/visual/incentivador_index_pj.php?perfil=representante_pj_resultado_busca',
+	9 => '/promac/visual/incentivador_index_pj.php?perfil=arquivos_incentivador_pj'
 );
 
 
 for ($i = 0; $i < count($urlIncentivadorPj); $i++) {
     if ($uri == $urlIncentivadorPj[$i]) {
-        if ($i == 0|| $i == 1) {
-            $liga1 = 'active loading';
+        if ($i == 0|| $i == 1) { // Cadastro Incentivador
+            $liga1 = 'active loading'; 
+        }elseif ($i == 2) { 	// Proponente
+        	$liga2 = 'active loading'; 
+        }elseif ($i == 3 || $i == 4 || $i == 5 || $i == 6 || $i == 7) { // Representante Legal
+        	$liga3 = 'active loading';
+        }elseif ($i == 10) { // Documentos do Incentivador
+        	$liga4 = 'active loading';
         }
 ?>
 
 	<!-- SmartWizard html -->
-        <div id="smartwizard">
-            <ul>
-                <li class="hidden">
-                    <a href=""><br /></a>
-                </li>
-                <li class="<?php echo isset($liga1) ? $liga1 : 'clickable'; ?>">
-                    <a onclick="location.href='incentivador_index_pj.php'" href=""><br />Incentivador</a>
-                </li>
-                <li class="<?php echo isset($liga2) ? $liga2 : 'clickable'; ?>">
-                    <a onclick="location.href=''" href=""><br />Representante Legal</a>
-                </li>
-                <li class="<?php echo isset($liga3) ? $liga3 : 'clickable'; ?>">
-                    <a onclick="location.href=''" href=""><br />Documentos do Incentivador</a>
-                </li>
-            </ul>
-        </div>
+    <div id="smartwizard">
+        <ul>
+            <li class="hidden">
+                <a href=""><br /></a>
+            </li>
+            <li class="<?php echo isset($liga1) ? $liga1 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pj.php'" href=""><br />Incentivador</a>
+            </li>
+            <li class="<?php echo isset($liga2) ? $liga2 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pf.php?perfil=incentivador_informacoes_iniciais_pj'" href=""><br />Proponente</a>
+            </li>
+            <li class="<?php echo isset($liga3) ? $liga3 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pj.php?perfil=incentivador_representante_pj'" href=""><br />Representante Legal</a>
+            </li>
+            <li class="<?php echo isset($liga4) ? $liga4 : 'clickable'; ?>">
+                <a onclick="location.href='incentivador_index_pj.php?perfil=arquivos_incentivador_pj'" href=""><br />Documentos do Incentivador</a>
+            </li>
+        </ul>
+    </div>
 <?php  
 	}
 }
