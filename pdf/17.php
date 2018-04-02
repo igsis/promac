43eb713fb58bcb043f99d0c87dc6f540f94f56cf
Lorelei Gabriele <loreleigab@hotmail.com>
2018-04-01 23:38:35 -0300

    Modelo Sugestivo de Carta de Intenção

---
+++ pdf/17.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-type: application/vnd.ms-word");
+header("Content-Disposition: attachment;Filename=carta_intencao.doc");
+?>
+
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<body>
+		<h4 align="center">MODELO SUGESTIVO DE CARTA DE INTENÇÃO</h4>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p align="justify">A empresa incentivadora ___________, sediada à __________________. São Paulo/SP, contribuinte do C.C.M.  nº _________ vem por meio desta afirmar a intenção de patrocinar no valor de R$______ (_______ mil reais) em ____  vez(ES) o projeto “_________” aprovado no Programa Municipal de Apoio a Projetos Culturais, para o imposto ________.</p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p><center>São Paulo, __ de ____ de 2018.</center></p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p align="center">_____________________________________<br/>
+			<strong>Proponente</strong></p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p align="center">_____________________________________<br/>
+			<strong>Incentivador</strong></p>
+</body>
+</html>
+
