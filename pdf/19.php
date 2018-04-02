8643ec09002674d69e9ccdffdb6fbb5f30eb1dca
LoreleiGab <loreleigab@hotmail.com>
2018-04-02 12:28:18 -0300

    Mudança nos nosmes dos PDF

---
+++ pdf/19.php
@@ -0,0 +1,19 @@
+<?php
+// GERANDO O WORD:
+header("Content-type: application/vnd.ms-word");
+header("Content-Disposition: attachment;Filename=carta_anuencia_local.doc");
+?>
+
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<body>
+		<h4 align="center">CARTA(S) DE ANUÊNCIA DO(S) LOCAL(IS)</h4>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p align="justify">Eu, ......................., RG nº .............., CPF nº.............., representante do espaço ......................., sediado no endereço ..............................., bairro ......................., CEP ...................., município de .............................................., declaro que tenho interesse em disponibilizar o referido espaço, caso o projeto denominado “.........................................” seja contemplado.</p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p align="center">_____________________________________<br/>
+			<strong>Responsável pelo espaço</strong></p>
+	</body>
+</html>
