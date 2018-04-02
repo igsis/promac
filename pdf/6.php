8fc553bbc3d29895c991cc4ab2f5762bbaa4d327
Lorelei Gabriele <loreleigab@hotmail.com>
2018-04-01 23:55:22 -0300

    Declaração de inscrição de proponente pf

--- pdf/6.php
+++ pdf/6.php
@@ -13,45 +13,6 @@ session_start();
 class PDF extends FPDF
 {
 
-// Simple table
-function BasicTable($header, $data)
-{
-    // Header
-    foreach($header as $col)
-        $this->Cell(40,7,$col,1);
-    $this->Ln();
-    // Data
-    foreach($data as $row)
-    {
-	       foreach($row as $col)
-            $this->Cell(40,6,$col,1);
-        $this->Ln();
-    }
-}
-
-// Simple table
-function Cabecalho($header, $data)
-{
-    // Header
-    foreach($header as $col)
-        $this->Cell(40,7,$col,1);
-    $this->Ln();
-    // Data
-
-}
-
-// Simple table
-function Tabela($header, $data)
-{
-    //Data
-    foreach($data as $col)
-        $this->Cell(40,7,$col,1);
-    $this->Ln();
-    // Data
-
-}
-
-
 }
 
 //CONSULTA
@@ -76,96 +37,78 @@ $pdf = new PDF('P','mm','A4'); //CRIA UM NOVO ARQUIVO PDF NO TAMANHO A4
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
-   
 $x=20;
-$l=6; //DEFINE A ALTURA DA LINHA   
-   
-$pdf->SetXY( $x , 15 );// SetXY - DEFINE O X (largura) E O Y (altura) NA PÁGINA
+$l=7; //DEFINE A ALTURA DA LINHA
 
+$pdf->SetXY( $x , 30 );// SetXY - DEFINE O X (largura) E O Y (altura) NA PÁGINA
 
-   
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','B', 14);
-   $pdf->Cell(180,5,utf8_decode("DECLARAÇÃO DE INSCRIÇÃO DE PROPONENTE"),0,1,'C');
-   
-   $pdf->Ln();
-   $pdf->Ln();
-   
-  
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("Eu, "."$Nome".", RG nº"."$RG".", CPF nº "."$CPF".", residente no endereço "."$Endereco".", "."$Numero".", bairro "."$Bairro".", CEP "."$Cep".", município de "."$Cidade"."."));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','B', 14);
+$pdf->Cell(180,5,utf8_decode("DECLARAÇÃO DE INSCRIÇÃO DE PROPONENTE"),0,1,'C');
 
-   $pdf->Ln();
+$pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("a) Tenho ciência e concordo com os termos da Lei nº 15.948/2013, do Decreto nº 58.041/2017 e desta portaria;"));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("Eu, ".$Nome.", RG nº ".$RG.", CPF nº ".$CPF.", residente no endereço ".$Endereco.", ".$Numero.", bairro ".$Bairro.", CEP ".$Cep.", município de ".$Cidade."."));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("b) As informações prestadas são verídicas;"));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("a) Tenho ciência e concordo com os termos da Lei nº 15.948/2013, do Decreto nº 58.041/2017 e desta portaria;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("c) Sou residente / sediado no Município de São Paulo há no mínimo 02 (dois) anos;"));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("b) As informações prestadas são verídicas;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("d) Não possuo qualquer associação ou vínculo direto ou indireto com empresas de serviços de radiodifusão de som e imagem ou operadoras de comunicação eletrônica aberta ou por assinatura;"));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("c) Sou residente / sediado no Município de São Paulo há no mínimo 02 (dois) anos;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("e) Não possuo qualquer associação ou vínculo direto ou indireto com o contribuinte incentivador do projeto apresentado, ressalvadas as hipóteses a que aludem o inciso XX do artigo 4º e o inciso III do artigo 7º da Lei nº15.948, de 2013;"));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("d) Não possuo qualquer associação ou vínculo direto ou indireto com empresas de serviços de radiodifusão de som e imagem ou operadoras de comunicação eletrônica aberta ou por assinatura;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("f) Estou em situação regular perante a Administração Pública;"));
-   $pdf->Ln();
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("e) Não possuo qualquer associação ou vínculo direto ou indireto com o contribuinte incentivador do projeto apresentado, ressalvadas as hipóteses a que aludem o inciso XX do artigo 4º e o inciso III do artigo 7º da Lei nº15.948, de 2013;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("g) Não estou cadastrado e não possuo débitos junto à Fazenda do Município de São Paulo;"));
-   $pdf->Ln();
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("f) Estou em situação regular perante a Administração Pública;"));
 
-   $pdf->Ln();
+$pdf->Ln();
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->MultiCell(170,$l,utf8_decode("h) O projeto apresentado não recebeu e nem recebe recursos de editais da Secretaria Municipal de Cultura."));
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("g) Não estou cadastrado e não possuo débitos junto à Fazenda do Município de São Paulo;"));
+$pdf->Ln();
 
-   $pdf->Ln();
-   $pdf->Ln();
-   $pdf->Ln();
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->MultiCell(180,$l,utf8_decode("h) O projeto apresentado não recebeu e nem recebe recursos de editais da Secretaria Municipal de Cultura."));
 
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->Cell(128,$l,utf8_decode("_______________________________"),0,1,'L');
-   $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 11);
-   $pdf->Cell(128,$l,utf8_decode(""."$Nome".""),0,1,'L');
-  
+$pdf->Ln();
+$pdf->Ln();
+$pdf->Ln();
 
-   $pdf->Ln();
-   $pdf->Ln();
-   $pdf->Ln();
-
+$pdf->SetX($x);
+$pdf->SetFont('Arial','', 11);
+$pdf->Cell(180,$l,utf8_decode("_________________________________________"),0,1,'C');
+$pdf->SetX($x);
+$pdf->Cell(180,$l,utf8_decode($Nome),0,1,'C');
 
 $pdf->Output();
-
-
 ?>
