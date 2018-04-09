b61a8b23571ad8d36e2e734f8ce8c83e407bd39e
LoreleiGab <loreleigab@hotmail.com>
2018-04-09 14:01:29 -0300

    Alteração na ordem dos itens do regulamento

--- index.php
+++ index.php
@@ -76,13 +76,13 @@
 										<ul class="list-group">
 											<li class="list-group-item"><a href="pdf/Lei_ProMac.pdf" target="_blank">Lei</a></li>
 											<li class="list-group-item"><a href="pdf/Decreto_ProMac.pdf" target="_blank">Decreto</a></li>
-											<li class="list-group-item"><a href="pdf/edital_promac.pdf" target="_blank">Edital de Abertura</a></li>
-											<li class="list-group-item"><a href="pdf/AlteracaoDecreto_ProMac.pdf" target="_blank">Alteração de Decreto</a></li>
+											<li class="list-group-item"><a href="pdf/AlteracaoDecreto_ProMac.pdf" target="_blank">Alteração de Decreto</a></li><li class="list-group-item"><a href="pdf/edital_promac.pdf" target="_blank">Edital de Abertura</a></li>
 											<li class="list-group-item"><a href="pdf/Portaria_publicado.pdf">Portaria de procedimentos e documentos para inscrição</a></li>
 											<li class="list-group-item"><a href="#">Portarias de prestação de contas</a></li>
 											<li class="list-group-item"><a href="pdf/manual_utilizacao.pdf" target="_blank">Manual de utilização do sistema</a></li>
 											<li class="list-group-item"><a href="pdf/identidade_visual.zip">Manual de uso da marca</a></li>
 											<li class="list-group-item"><a href="#">Manual de prestação de contas</a></li>
+											<li class="list-group-item"><a href="pdf/edital_promac.pdf" target="_blank">Edital de Abertura</a></li>
 										</ul>
 									</div>
 									<div class="modal-footer">
