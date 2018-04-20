2c18bddd8acf32c2e869caf2d9b25815510b29ca
Lorelei Gabriele <loreleigab@hotmail.com>
2018-04-20 18:15:33 -0300

    Início da tela de reaprovação do cadastro de pf

---
+++ perfil/smc_reaprova_pf.php
@@ -0,0 +1,86 @@
+<?php
+$con = bancoMysqli();
+$tipoPessoa = '1';
+
+$idPf = $_POST['idPf'];
+$pf = recuperaDados("pessoa_fisica","idPf",$idPf);
+
+if(isset($_POST['reaprovar']))
+{
+	$idPf = $_POST['idPf'];
+	$sql = "UPDATE pessoa_fisica SET liberado = 3 WHERE idPf = '$idPf'";
+	if(mysqli_query($con,$sql_liberacao))
+	{
+		$mensagem = "Cadastro aprovado com sucesso!"
+	}
+	else
+	{
+		$mensagem = "Erro ao aprovar cadastro. Por favor, tente novamente!";
+	}
+}
+?>
+
+<section id="list_items" class="home-section bg-white">
+	<div class="container"><?php include 'includes/menu_smc.php'; ?>
+		<div class="form-group">
+			<div class="alert alert-warning">
+				<strong>Atenção!</strong> Confirme atentamente se os dados abaixo estão corretos!
+			</div>
+		</div>
+		<div class = "page-header">
+		 	<h5>Dados do proponente</h5>
+		 	<h4><?php if(isset($mensagem)) echo $mensagem; ?></h4>
+		</div>
+		<div class="well">
+			<p align="justify"><strong>Nome:</strong> <?php echo isset($pf['nome']) ? $pf['nome'] : null; ?></p>
+			<p align="justify"><strong>CPF:</strong> <?php echo isset($pf['cpf']) ? $pf['cpf'] : null; ?><p>
+			<p align="justify"><strong>RG:</strong> <?php echo isset($pf['rg']) ? $pf['rg'] : null; ?><p>
+			<p align="justify"><strong>Telefone:</strong> <?php echo isset($pf['telefone']) ? $pf['telefone'] : null; ?></p>
+			<p align="justify"><strong>Celular:</strong> <?php echo isset($pf['celular']) ? $pf['celular'] : null; ?></p>
+			<p align="justify"><strong>Email:</strong> <?php echo isset($pf['email']) ? $pf['email'] : null; ?></p>
+			<p align="justify"><strong>Logradouro:</strong> <?php echo isset($pf['logradouro']) ? $pf['logradouro'] : null; ?></p>
+			<p align="justify"><strong>Número:</strong> <?php echo isset($pf['numero']) ? $pf['numero'] : null; ?></p>
+			<p align="justify"><strong>Complemento:</strong> <?php echo isset($pf['complemento']) ? $pf['complemento'] : null; ?></p>
+			<p align="justify"><strong>Bairro:</strong> <?php echo isset($pf['bairro']) ? $pf['bairro'] : null; ?></p>
+			<p align="justify"><strong>Cidade:</strong> <?php echo isset($pf['cidade']) ? $pf['cidade'] : null; ?></p>
+			<p align="justify"><strong>Estado:</strong> <?php echo isset($pf['estado']) ? $pf['estado'] : null; ?></p>
+			<p align="justify"><strong>CEP:</strong> <?php echo isset($pf['cep']) ? $pf['cep'] : null; ?></p>
+		</div>
+		<div class="table-responsive list_info">
+			<h6>Arquivo(s) de Pessoa Física</h6>
+			<?php
+				exibirArquivos($tipoPessoa,$idPf);
+			?>
+		</div>
+	</div>
+
+<!-- Botão para Prosseguir -->
+	<div class="form-group">
+		<div class='col-md-offset-4 col-md-2'>
+			<!-- Button para ativar modal -->
+			<button type="button" class='btn btn-theme btn-lg btn-block' data-toggle="modal" data-target="#myModal">
+				Aprovar
+			</button>
+		</div>
+	</div>
+	<!-- Modal de aviso -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title text-danger" id="myModalLabel">Atenção</h4>
+				</div>
+				<div class="modal-body">
+					<p>Tem certeza que deseja aprovar um cadastro que não está liberado?</p>
+				</div>
+				<div class="modal-footer">
+					<form class='form-horizontal' role='form' action='?perfil=smc_visualiza_pf' method='post'>
+						<input type='hidden' name='idPf' value='<?php echo $pf['idPf'] ?>' />
+						<input type='submit' name='negar' value='Não Aprovar' class='btn btn-theme btn-lg btn-block'>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
