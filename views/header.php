0d982c673773cb32b7eff056736a5bed54955cc7
Leandro Soares Ribeiro <x028641@rede.sp>
2018-04-26 17:54:34 -0300

    Monitoramento PF

---
+++ views/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>    
+    <title>SMC / Pro-Mac - Programa Municipal de Apoio a Projetos Culturais</title>    
+    <meta charset="utf-8">    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">        
+    <link rel="stylesheet" type="text/css" href="../views/css/stilo.css">    
+  </head>
+  <body>
+    <div class="container">          
+      
+      
