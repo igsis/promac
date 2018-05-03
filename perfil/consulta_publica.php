2795c7f0ccd03e78a94e59948514bc8e7195826c
LoreleiGab <loreleigab@hotmail.com>
2018-05-03 13:38:57 -0300

    Mudança no visual da tela inicial de consulta pública

--- perfil/consulta_publica.php
+++ perfil/consulta_publica.php
@@ -4,7 +4,7 @@
 			<div class="form-group">
 				<div class="col-md-offset-2 col-md-8">
 					<strong>
-						<a href="../include/logoff.php">Voltar a Tela de Login</a>
+						<a href="../include/logoff.php">Voltar a Tela de Início</a>
 					</strong>
 				</div>
 			</div>
@@ -17,80 +17,85 @@
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
 				<?php include 'includes/menu_consulta_publica.php'; ?>
-				<div class="well">
-					Nesta área, você pode consultar os projetos já aprovados!
-				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
 				<form method="POST" action="?perfil=consulta_publica_resultado" class="form-horizontal" role="form">
 					<hr/>
+					<div class="well">
 
-					<label>PESSOA FÍSICA</label>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-5"><label>Nome</label>
-							<input type="text" name="nome" class="form-control" placeholder="">
+						<div class="alert-success">
+							<p>&nbsp;</p>
+							<p><strong>Nesta área, você pode consultar os projetos já aprovados!</strong></p>
+							<p>&nbsp;</p>
 						</div>
-						<div class="col-md-3"><label>CPF</label>
-							<input type="text" name="cpf" id="cpf" class="form-control" placeholder="">
-						</div>
-					</div>
-					<hr/>
 
-					<label>PESSOA JURÍDICA</label>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-5"><label>Razão Social</label>
-							<input type="text" name="razaoSocial" class="form-control" placeholder="">
+						<label>PESSOA FÍSICA</label>
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-5"><label>Nome</label>
+								<input type="text" name="nome" class="form-control" placeholder="">
+							</div>
+							<div class="col-md-3"><label>CPF</label>
+								<input type="text" name="cpf" id="cpf" class="form-control" placeholder="">
+							</div>
 						</div>
-						<div class="col-md-3"><label>CNPJ</label>
-							<input type="text" name="cnpj" id="cnpj" class="form-control" placeholder="">
-						</div>
-					</div>
-					<hr/>
+						<hr/>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><label>Nome do projeto</label>
-							<input type="text" name="nomeProjeto" class="form-control" placeholder="">
+						<label>PESSOA JURÍDICA</label>
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-5"><label>Razão Social</label>
+								<input type="text" name="razaoSocial" class="form-control" placeholder="">
+							</div>
+							<div class="col-md-3"><label>CNPJ</label>
+								<input type="text" name="cnpj" id="cnpj" class="form-control" placeholder="">
+							</div>
 						</div>
-					</div>
+						<hr/>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><label>Protocolo</label>
-							<input type="text" name="protocolo" class="form-control" placeholder="">
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8"><label>Nome do projeto</label>
+								<input type="text" name="nomeProjeto" class="form-control" placeholder="">
+							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><label>Status</label>
-							<select class="form-control" name="idStatus" >
-								<option value="0"></option>
-			                    <option value="5">Aprovado</option>
-   			                    <option value="6">Reprovado</option> 
-			                    <option value="7">Inscrito</option> 
-							</select>
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8"><label>Protocolo</label>
+								<input type="text" name="protocolo" class="form-control" placeholder="">
+							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><label>Área atuação</label>
-							<select class="form-control" name="idAreaAtuacao" >
-								<option value="0"></option>
-								<?php echo geraOpcao("area_atuacao","") ?>
-							</select>
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8"><label>Status</label>
+								<select class="form-control" name="idStatus" >
+									<option value="0"></option>
+				                    <option value="5">Aprovado</option>
+	   			                    <option value="6">Reprovado</option> 
+				                    <option value="7">Inscrito</option> 
+								</select>
+							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><label>Valor Aprovado</label>
-							<input type="text" name="valorAprovado" class="form-control" placeholder="">
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8"><label>Área atuação</label>
+								<select class="form-control" name="idAreaAtuacao" >
+									<option value="0"></option>
+									<?php echo geraOpcao("area_atuacao","") ?>
+								</select>
+							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<input type="hidden" name="consulta" value="1">
-							<input type="submit" name="pesquisar" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8"><label>Valor Aprovado</label>
+								<input type="text" name="valorAprovado" class="form-control" placeholder="">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-offset-2 col-md-8">
+								<input type="hidden" name="consulta" value="1">
+								<input type="submit" name="pesquisar" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+							</div>
 						</div>
 					</div>
 				</form>
