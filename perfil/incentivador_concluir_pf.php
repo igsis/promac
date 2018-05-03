95d4ed1b9018554451edd45bcd221547a66ecd64
Isaias Mendes <isaiaszmendes@gmail.com>
2018-05-03 11:55:31 -0300

    Resultado incentivador pf

--- perfil/incentivador_concluir_pf.php
+++ perfil/incentivador_concluir_pf.php
@@ -47,98 +47,25 @@ if(isset($_POST['liberacao']))
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
 			<?php
-				if($pf['liberado'] == 0) // ainda não foi solicitado liberação
-				{
-					include 'includes/resumo_incentivador_pf.php';
-				?>
-					<div class="alert alert-info">
-						Após o preenchimento de todos os dados pessoais, conclua a inscrição do proponente e aguarde a análise da sua documentação pela Secretaria Municipal de Cultura.
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<?php							
-							if ($cpo == false)							{
-								
-								$idPess = $pf['idPf'];
-								$queryArquivos = 
-								  "SELECT 
-								    idUploadArquivo 
-								   FROM 
-								     upload_arquivo 
-								   WHERE 
-								     idPessoa = $idPess 
-								   AND idTipo = '4' AND publicado = '1'";
-								
-								$enviaArquivos = mysqli_query($con, $queryArquivos);
-								$numRow = mysqli_num_rows($enviaArquivos);
-								if($numRow == 5)
-								{?>
-							<form class="form-horizontal" role="form" action="?perfil=incentivador_concluir_pf" method="post">
-								<input type="submit" name="liberacao" value="Concluir inscrição do Incentivador" class="btn btn-theme btn-lg btn-block">
-							</form>
-							<?php
-							}
-							else{
-								echo "<div class='alert alert-warning'>
-								<strong>Erro: </strong> Você deve enviar todos os documentos para prosseguir.
-								</div>";
-							}
-						}?>
-						</div>
-					</div>
-			</div>
+				
+			if($pf['liberado'] == 3)// liberação concedida pela SMC - liberado = 3
+			{
+			?>
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">Sua inscrição foi aprovada pela SMC.</div><br/>
+				</div>
 			<?php
-				}
-				elseif($pf['liberado'] == 1)// foi solicitado liberação, porém a SMC não analisou ainda.
-				{
+				include 'includes/resumo_incentivador_pf.php';
+			}else {
 			?>
-					<div class="alert alert-success">
-						<strong>Sua solicitação de inscrição foi enviada com sucesso à Secretaria Municipal de Cultura. Aguarde a análise da documentação.</strong>
+				<div class="form-group">
+					<div class="alert alert-warning">
+						<div class="col-md-offset-2 col-md-8">Inscrição em andamento.</div><br/>
 					</div>
+				</div>
 			<?php
-				}
-				elseif(($pf['liberado'] == 2) || ($pf['liberado'] == 4))// a liberação de projetos foi rejeitada pela SMC.
-				{
-					if ($pf['liberado'] == 2) 
-					{
+			}
 			?>
-					<div class="alert alert-danger">
-						<strong>Sua inscrição para incentivo foi rejeitada pela Secretaria Municipal de Cultura.</strong>
-					</div>
-			<?php 
-					}
-					else
-					{
-			?>
-					<div class="alert alert-danger">
-						<strong>Sua inscrição para incentivo foi liberada para edição.</strong>
-					</div>
-			<?php 
-					}
-			?>
-
-					<div>
-				 		<?php listaArquivosPessoaObs($idPf,4) ?>
-				 	</div>
-				 	<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<form class="form-horizontal" role="form" action="?perfil=projeto_pf" method="post">
-								<input type="submit" name="liberacao" value="Concluir inscrição do Incentivador" class="btn btn-theme btn-lg btn-block">
-							</form>
-						</div>
-					</div>
-			<?php
-				}
-				else // liberação concedida pela SMC - liberado = 3
-				{
-			?>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">Sua inscrição foi aprovada pela SMC.</div><br/>
-					</div>
-				<?php
-					include 'includes/resumo_incentivador_pf.php';
-				}
-				?>
 		</div>
 		<!-- Confirmação de Exclusão -->
 			<div class="modal fade" id="confirmApagar" role="dialog" aria-labelledby="confirmApagarLabel" aria-hidden="true">
