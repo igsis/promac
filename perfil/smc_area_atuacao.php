275bd7cb380f577124a23ebfbd9008390357d448
Isaias Mendes <isaiaszmendes@gmail.com>
2018-05-07 11:46:23 -0300

    Area de atuação

--- perfil/smc_area_atuacao.php
+++ perfil/smc_area_atuacao.php
@@ -1,14 +1,15 @@
 <?php 
 $conn = bancoPDO();
 
-$sql = "SELECT * FROM area_atuacao";
-
 if (isset($_POST['atualizar']))
 {
 	$id = $_POST['id'];
 	$area = $_POST['area'];
-	$query = "UPDATE area_atuacao SET areaAtuacao = '".$area."' WHERE idArea = '".$id."'";
-	if($conn->query($query))
+	$tipo = $_POST['tipoPessoa'];
+
+	$sql_atualiza = "UPDATE area_atuacao SET areaAtuacao = '".$area."', tipo = '".$tipo."' WHERE idArea = '".$id."'";
+	$stmt = $conn->prepare($sql_atualiza);
+	if($stmt->execute())
 	{
 		$mensagem = "Atualizado com sucesso!";
 	}else {
@@ -16,6 +17,43 @@ if (isset($_POST['atualizar']))
 	}
 	
 }
+
+if (isset($_POST['apagar']))
+{
+	$id = $_POST['id'];
+	$sql_deleta = "DELETE FROM area_atuacao WHERE idArea = :id ";
+	$stmt = $conn->prepare($sql_deleta);
+	$stmt->bindParam(':id', $id);
+	
+	if ($stmt->execute())
+	{
+		$mensagem = "Área removida com sucesso!";
+	}else {
+		$mensagem = "Erro ao remover!";
+	}
+	
+}
+
+if (isset($_POST['inserir'])) 
+{
+	$conn = bancoPDO();
+	$area = $_POST['nomeArea'];
+	$tipo = $_POST['tipoPessoa'];
+
+	$sql_insere = "INSERT INTO `area_atuacao` (`areaAtuacao`, `tipo`) VALUES (:areaAtuacao,:tipo)";
+	$stmt = $conn->prepare($sql_insere);
+	$stmt->bindParam(':areaAtuacao',$area);
+	$stmt->bindParam(':tipo',$tipo);
+
+	if ($stmt->execute()) 
+	{
+		$mensagem = "Área inserida com sucesso!";
+	}else {
+		$mensagem = "Erro ao inserir!";
+	}
+
+}
+$sql = "SELECT * FROM area_atuacao";
 ?>
 <section id="list_items" class="home-section bg-white">
 	<div class="container"><?php include 'includes/menu_smc.php'; ?>
@@ -36,7 +74,8 @@ if (isset($_POST['atualizar']))
                 </thead> 
                 <tbody>
 <?php
-            foreach ($conn->query($sql) as $i => $row) {
+            foreach ($conn->query($sql) as $i => $row) 
+            {
             	if ($row['tipo'] == 1) {
             		$pessoa = 'Pessoa Física';
             	}else{
@@ -46,21 +85,22 @@ if (isset($_POST['atualizar']))
                 echo    "<th scope='row'>$i</th>";
                 echo    "<td class='list_description'>".$row['areaAtuacao']."</td>";
                 echo    "<td class='list_description'>".$pessoa."</td>";
-                            // <form action='' method='post'>
+                            // <form action='' method='post'></form>
                 echo    "<td class='list_description'>
-                                <button type='submit' name='edita' class='btn btn-info btn-sm' data-toggle='modal' data-target='#$i'><i class='fa fa-pencil'></i>
+                                <button type='submit' name='edita' class='btn btn-info btn-sm' data-toggle='modal' data-target='#edita$i'><i class='fa fa-pencil'></i>
                                 </button>
-                                <button type='submit' name='apaga' class='btn btn-danger btn-sm'><i class='fa fa-trash-o fa-lg'></i>
+                                <button type='submit' name='apaga' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#apaga$i'><i class='fa fa-trash-o fa-lg'></i>
                                 </button>
-                            </form>
+                            
                         </td>";
                 echo "</tr>";
+                // Modal Edição 
                 echo "
-					<div class='modal fade' id='$i' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+					<div class='modal fade' id='edita$i' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
 					  <div class='modal-dialog modal-dialog-centered' role='document'>
 					    <div class='modal-content'>
 					      <div class='modal-header'>
-					        <h5 class='modal-title' id=''>Área</h5>
+					        <h5 class='modal-title' id=''>Editar Área</h5>
 					        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
 					          <span aria-hidden='true'>&times;</span>
 					        </button>
@@ -68,8 +108,39 @@ if (isset($_POST['atualizar']))
 					      <div class='modal-body'>
 					      	<form action='' method='post' class='form-group'>
 								<input type='hidden' name='id' value='".$row['idArea']."' class='form-control'><br>
-								<input type='text' name='area' value='".$row['areaAtuacao']."' class='form-control'><br>
+								<input type='text' name='area' value='".$row['areaAtuacao']."' class='form-control'>
+								<select name='tipoPessoa'>
+									<option value=".$row['tipo']."></option>
+									<option value='1'>Pessoa Física</option>
+									<option value='2'>Pessoa Jurídica</option>
+								</select>
+								<br><br>
 								<input type='submit' name='atualizar' class='btn btn-theme btn-block' value='Savar'>
+					      	</form>
+					      </div>
+					      <div class='modal-footer'>
+					        <button type='button' class='btn btn-secondary' data-dismiss='modal'>Fechar</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>";
+				// Modal Apagar
+				echo "
+					<div class='modal fade' id='apaga$i' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+					  <div class='modal-dialog modal-dialog-centered' role='document'>
+					    <div class='modal-content'>
+					      <div class='modal-header'>
+					        <h5 class='modal-title' id=''>Quer remover essa área?</h5>
+					        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+					          <span aria-hidden='true'>&times;</span>
+					        </button>
+					      </div>
+					      <div class='modal-body'>
+					      	<form action='' method='post' class='form-group'>
+								<p>".$row['areaAtuacao']."</p>
+								<input type='hidden' name='id' value='".$row['idArea']."' class='form-control'><br>	
+								<input type='submit' name='apagar' class='btn btn-theme ' value='Sim'>
+								<button type='button' class='btn btn-theme' data-dismiss='modal'>Não</button>
 					      	</form>
 					      </div>
 					      <div class='modal-footer'>
@@ -82,13 +153,12 @@ if (isset($_POST['atualizar']))
             	echo "<form action='' method='post' class='form-group'>";
              	echo "<tr>
 	                    <th scope='row'>$i</th>
-	                	    <td><input type='text' name='' value='' class='form-control'></td>
+	                	    <td><input type='text' name='nomeArea' value='' class='form-control'></td>
 	                	 	<td><select name='tipoPessoa'>
-	                	 		<option value=''></option>
 	                	 		<option value='1'>Pessoa Física</option>
 	                	 		<option value='2'>Pessoa Física e Júridica</option>
 	                	 	</select></td>
-	                	    <td><button type='submit' class='btn btn-success'><i class='fa fa-plus'></i></i>
+	                	    <td><button type='submit' name='inserir' class='btn btn-success '><i class='fa fa-plus'></i></i>
 							</button></td>
 	                 	</tr>";
                 echo "</form>";
