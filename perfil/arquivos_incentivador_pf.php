9098a239cab54aa08dda2e3e050d61f2e498ed5d
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-06-04 13:43:42 -0300

    Arquivos - Incentivador PF (reparo)

--- perfil/arquivos_incentivador_pf.php
+++ perfil/arquivos_incentivador_pf.php
@@ -93,7 +93,7 @@ $pf = recuperaDados("incentivador_pessoa_fisica","idPf",$idPf);
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
 				<?php 
-				if($pf['liberado'] == NULL OR $pf['liberado'] == 2 OR $pf['liberado'] == 4)
+				if($pf['liberado'] == NULL)
 				{
 				?>
 				<!-- Exibir arquivos -->
