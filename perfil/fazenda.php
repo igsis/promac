66686e686b78dc92e9faa34f6ad9f0cc3c589c2e
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2018-06-20 15:01:24 -0300

    Item Fazenda

--- perfil/fazenda.php
+++ perfil/fazenda.php
@@ -10,24 +10,30 @@ function calculaValorReservaNaoLiquidada($valorDaReserva, $valorDaLiquidacao){
 	return $valorReservaNaoLiquidada = $valorDaReserva - $valorDaLiquidacao;
 }
 ?>
-<section id="list_items" class="home-section bg-white">
-    <div class="container"><?php include 'includes/menu_smc.php'; ?>
-        <div class="form-group">
-            <h4>Fazenda</h4>
-        </div>
-<div class="col-md-offset-1 col-md-10">
-        <div class="table-responsive list_info">
-				<?php
-					$sql_financeiro = "SELECT * FROM financeiro WHERE publicado = 1";
-					$query_financeiro = mysqli_query($con, $sql_financeiro);
+    <section id="list_items" class="home-section bg-white">
+        <div class="container">
+            <?php include 'includes/menu_smc.php'; ?>
+            <div class="form-group">
+                <h4>Fazenda</h4>
+            </div>
+            <div class="col-md-offset-1 col-md-10">
+                <div class="table-responsive list_info">
+                    <?php
+					$sql_reserva = "SELECT * FROM reserva";
+					$query_reserva = mysqli_query($con, $sql_reserva);
 					$valorReserva = 0;
-					$valorLiquidacao = 0;
-					while($campo = mysqli_fetch_array($query_financeiro)){
-						$valorReserva += $campo['valorReserva'];
-						$valorLiquidacao += $campo['valorLiquidacao'];
+					while($campo = mysqli_fetch_array($query_reserva)){
+						$valorReserva += $campo['valor'];
 					}
-
-					$sql = "SELECT * FROM orcamento_anual LIMIT 0,10";
+                    
+                    $sql_liquidacao = "SELECT * FROM liquidacao";
+                    $query_liquidacao = mysqli_query($con, $sql_liquidacao);
+                    $valorLiquidacao = 0;
+                    while($campo = mysqli_fetch_array($query_liquidacao)){
+                        $valorLiquidacao += $campo['valor'];
+                    }
+                    
+					$sql = "SELECT * FROM orcamento_anual ORDER BY ano DESC LIMIT 0,10";
 					$query = mysqli_query($con,$sql);
 					$num = mysqli_num_rows($query);
 					if($num > 0)
@@ -48,9 +54,15 @@ function calculaValorReservaNaoLiquidada($valorDaReserva, $valorDaLiquidacao){
 								{
 									echo "<tr>";
 									echo "<td class='list_description'>".$campo['ano']."</td>";
-									echo "<td class='list_description'>R$ ".number_format($campo['valor'], 2, ',', '.')."</td>";
-									echo "<td class='list_description'>R$ ".number_format(calculaValorDisponivel($campo['valor'], $valorReserva), 2, ',', '.') ."</td>";
-									echo "<td class='list_description'>R$ ".number_format(calculaValorReservaNaoLiquidada($valorReserva, $valorLiquidacao), 2, ',', '.')."</td>";
+                                    
+									echo "<td class='list_description'>
+                                    R$ ".number_format($campo['valor'], 2, ',', '.')."</td>";
+                                    
+									echo "<td class='list_description'>
+                                    R$ ".number_format(calculaValorDisponivel($campo['valor'], $valorReserva), 2, ',', '.') ."</td>";
+                                    
+									echo "<td class='list_description'>
+                                    R$ ".number_format(calculaValorReservaNaoLiquidada($valorReserva, $valorLiquidacao), 2, ',', '.')."</td>";
 								}
 							echo "</tr>";
 							echo "</tbody>
@@ -61,7 +73,7 @@ function calculaValorReservaNaoLiquidada($valorDaReserva, $valorDaLiquidacao){
 							echo "Não há resultado no momento.";
 						}
 				?>
-			</div>
-	</div>
-	</div>
-</section>
+                </div>
+            </div>
+        </div>
+    </section>
