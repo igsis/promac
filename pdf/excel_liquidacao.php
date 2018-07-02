bf393c1fc68ec38a2f48570f3dc34b0d94dae249
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-07-02 18:46:10 -0300

    Relatório Excel - Liquidação [INCOMPLETO]

---
+++ pdf/excel_liquidacao.php
@@ -0,0 +1,108 @@
+<?php
+//require '../include/';
+require_once("../funcoes/funcoesConecta.php");
+require_once("../funcoes/funcoesGerais.php");
+require_once("../include/phpexcel/Classes/PHPExcel.php");
+
+//CONEXÃO COM BANCO DE DADOS
+$con = bancoMysqli();
+
+// Create new PHPExcel object
+$objPHPExcel = new PHPExcel();
+
+// Set document properties
+$cacheMethod = PHPExcel_CachedObjectStorageFactory:: cache_to_phpTemp;
+$cacheSettings = array( ' memoryCacheSize ' => '8MB');
+PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
+$objPHPExcel->getProperties()->setCreator("Sistema Pro-Mac");
+$objPHPExcel->getProperties()->setLastModifiedBy("Sistema Pro-Mac");
+$objPHPExcel->getProperties()->setTitle("Relatório de Liquidações");
+$objPHPExcel->getProperties()->setSubject("Relatório de Liquidações");
+$objPHPExcel->getProperties()->setDescription("Gerado automaticamente a partir do Sistema Pro-Mac");
+$objPHPExcel->getProperties()->setKeywords("office 2007 openxml php");
+$objPHPExcel->getProperties()->setCategory("Relatório de Liquidações");
+
+// Add some data
+$objPHPExcel->setActiveSheetIndex(0)
+    ->setCellValue('A1', 'Data')
+    ->setCellValue('B1', 'Valor')
+    ->setCellValue('C1', 'Número da Liquidação')
+    ->setCellValue('D1', 'ID Depósito');
+
+//Colorir a primeira fila
+$objPHPExcel->getActiveSheet()->getStyle('A1:D1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+$objPHPExcel->getActiveSheet()->getStyle('A1:D1')->getFill()->getStartColor()->setARGB('#29bb04');
+// Add some data
+$objPHPExcel->getActiveSheet()->getStyle("A1:D1")->getFont()->setBold(true);
+$objPHPExcel->getActiveSheet()->getStyle('A1:D1')->getBorders()->getAllBorders()->setBorderStyle(PHPExcel_Style_Border::BORDER_THIN);
+$styleArray = array(
+    'borders' => array(
+        'allborders' => array(
+            'style' => PHPExcel_Style_Border::BORDER_THIN
+        )
+    ),
+    'alignment' => array(
+        'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+    )
+);
+$objPHPExcel->getDefaultStyle()->applyFromArray($styleArray);
+
+$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+
+
+//Dados
+$sql = "SELECT
+          D.idDeposito,
+          L.data,
+          L.valor,
+          L.numeroLiquidacao,
+          L.idLiquidacao
+        FROM liquidacao AS L
+          LEFT JOIN deposito AS D ON D.idDeposito = L.idDeposito
+        ORDER BY L.idLiquidacao";
+$query = mysqli_query($con,$sql);
+$campo = mysqli_fetch_array($query);
+
+$i = 2; // para começar a gravar os dados na segunda linha
+while($linha = mysqli_fetch_array($query))
+{
+    $deposito = $objPHPExcel->getActiveSheet()->getCell('D'.($i-1))->getValue();
+
+    if ($deposito == $linha['idDeposito'])
+    {
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('D'.($i-1).':'.'D'.$i);
+    }
+
+    $objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue('A'.$i, $linha['data'])
+        ->setCellValue('B'.$i, $linha['valor'])
+        ->setCellValue('C'.$i, $linha['numeroLiquidacao'])
+        ->setCellValue('D'.$i, $linha['idDeposito']);
+    $i++;
+}
+
+$objPHPExcel->setActiveSheetIndex(0);
+ob_end_clean();
+ob_start();
+
+$nome_arquivo = date("Y-m-d H:i:s")."_liquidacao.xls";
+
+// Redirect output to a client’s web browser (Excel2007)
+header('Content-Type: text/html; charset=ISO-8859-1');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="'.$nome_arquivo.'"');
+header('Cache-Control: max-age=0');
+// If you're serving to IE 9, then the following may be needed
+header('Cache-Control: max-age=1');
+
+// If you're serving to IE over SSL, then the following may be needed
+header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+header ('Pragma: public'); // HTTP/1.0
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter->save('php://output');
