0a580d234209096760c92338ef4830b205d59113
Diego Vicente <diego.vdossantos@gmail.com>
2018-08-30 15:28:04 -0300

    acerto relatório ata reunião

--- pdf/ata_reuniao.php
+++ pdf/ata_reuniao.php
@@ -28,94 +28,84 @@ header("Content-Disposition: attachment;Filename=carta_intencao.doc");
 					INNER JOIN area_atuacao ON idAreaAtuacao = idArea 
 					WHERE `dataReuniao` = '$data' AND idStatus = 5"; //status aprovado
 		$query_area = mysqli_query($con,$sql_area);
-		$i = 0;
 		while($campo = mysqli_fetch_array($query_area))
 		{
-		    $x[$i]['area'] = $campo['areaAtuacao'];
-		    $area = $x[$i]['area'];
-			$sql = "SELECT * FROM projeto WHERE idAreaAtuacao = '$area' ORDER BY idAreaAtuacao";
-			$query = mysqli_query($con,$sql);
-			echo "<h4>".strtoupper($area)."</h4>";
+		    $x['area'] = $campo['areaAtuacao'];
+		    $x['idArea'] = $campo['idAreaAtuacao'];
+		    $area = $x['area'];
+		    $idArea = $x['idArea'];
+            echo "<h4>".strtoupper($area)."</h4>";
+            $sql = "SELECT
+                      p.nomeProjeto,
+                      p.protocolo,
+                      p.valorAprovado,
+                      rf.renunciaFiscal,
+                      pf.nome as 'nomePf',
+                      pj.razaoSocial as 'nomePj'
+                      FROM projeto as p
+                      INNER JOIN renuncia_fiscal as rf ON p.idRenunciaFiscal = rf.idRenuncia
+                      LEFT JOIN pessoa_fisica as pf ON p.idPf = pf.idPf
+                      LEFT JOIN pessoa_juridica as pj ON p.idPj = pj.idPj
+                      WHERE p.dataReuniao = '$data'
+                        AND p.idAreaAtuacao = '". $x['idArea']. "' AND p.idStatus = 5";
+            $query = mysqli_query($con,$sql);
 
-			while($projeto = mysqli_fetch_array($query))
+            foreach ($query as $projeto)
 			{
-			    $renuncia = recuperaDados("renuncia_fiscal","idRenuncia",$projeto['idRenunciaFiscal']);
-			    if($projeto['tipoPessoa'] == 1)
-			    {
-			    	$pf = recuperaDados("pessoa_fisica","idPf",$projeto['idPf']);
-			    	$projeto['proponente'] = $pf['nome'];
-			    }
-			    else
-			    {
-			    	$pj = recuperaDados("pessoa_juridica","idPj",$projeto['idPj']);
-			    	$projeto['proponente'] = $pj['razaoSocial'];
-			    }
-			    $projeto['nomeProjeto'] = $projeto['nomeProjeto'];
-			    $projeto['protocolo'] = $projeto['protocolo'];
-			    $projeto['valorAprovado'] = $projeto['valorAprovado'];
-			    $projeto['renuncia'] = $renuncia['renunciaFiscal'];
-
-			    echo "<p><strong>Nome do projeto:</strong> ".$projeto['proponente']."</p>";
-			    echo "<p><strong>Protocolo:</strong> ".$projeto['protocolo']."</p>";
-			    echo "<p><strong>Valor total aprovado:</strong> ".$projeto['valorAprovado']."</p>";
-			    echo "<p><strong>Percentual de renúncia:</strong> ".$projeto['renuncia']."</p>";
-			    echo "<p>&nbsp;</p>";
-			}
-			
-		    $i++;
+                echo "<p><strong>Nome do projeto:</strong> ".$projeto['nomeProjeto']."</p>";
+                echo "<p><strong>Protocolo:</strong> ".$projeto['protocolo']."</p>";
+                echo "<p><strong>Valor total aprovado:</strong> ".$projeto['valorAprovado']."</p>";
+                echo "<p><strong>Percentual de renúncia:</strong> ".$projeto['renunciaFiscal']."</p>";
+                echo "<p>&nbsp;</p>";
+            }
 		}
 		?>
 		<p>&nbsp;</p>
 		<p>&nbsp;</p>
 		<p align="justify">A CJP - Comissão Julgadora de Projetos Culturais decidiu <strong>REPROVAR</strong> os projetos abaixo relacionados, nos seguintes segmentos:</p>
 
-		<?php
-		$sql_area = "SELECT DISTINCT idAreaAtuacao, areaAtuacao FROM `projeto`
+        <?php
+        $sql_area = "SELECT DISTINCT idAreaAtuacao, areaAtuacao FROM `projeto`
 					INNER JOIN area_atuacao ON idAreaAtuacao = idArea 
 					WHERE `dataReuniao` = '$data' AND idStatus = 6"; //status reprovado
-		$query_area = mysqli_query($con,$sql_area);
-		$i = 0;
-		while($campo = mysqli_fetch_array($query_area))
-		{
-		    $x[$i]['area'] = $campo['areaAtuacao'];
-		    $area = $x[$i]['area'];
-			$sql = "SELECT * FROM projeto WHERE idAreaAtuacao = '$area' ORDER BY idAreaAtuacao";
-			$query = mysqli_query($con,$sql);
-			echo "<h4>".strtoupper($area)."</h4>";
+        $query_area = mysqli_query($con,$sql_area);
+        while($campo = mysqli_fetch_array($query_area))
+        {
+            $x['area'] = $campo['areaAtuacao'];
+            $x['idArea'] = $campo['idAreaAtuacao'];
+            $area = $x['area'];
+            $idArea = $x['idArea'];
+            echo "<h4>".strtoupper($area)."</h4>";
+            $sql = "SELECT
+                      p.nomeProjeto,
+                      p.protocolo,
+                      p.valorAprovado,
+                      rf.renunciaFiscal,
+                      pf.nome as 'nomePf',
+                      pj.razaoSocial as 'nomePj'
+                      FROM projeto as p
+                      INNER JOIN renuncia_fiscal as rf ON p.idRenunciaFiscal = rf.idRenuncia
+                      LEFT JOIN pessoa_fisica as pf ON p.idPf = pf.idPf
+                      LEFT JOIN pessoa_juridica as pj ON p.idPj = pj.idPj
+                      WHERE p.dataReuniao = '$data'
+                        AND p.idAreaAtuacao = '". $x['idArea']. "' AND p.idStatus = 6";
+            $query = mysqli_query($con,$sql);
 
-			while($projeto = mysqli_fetch_array($query))
-			{
-			    $renuncia = recuperaDados("renuncia_fiscal","idRenuncia",$projeto['idRenunciaFiscal']);
-			    if($projeto['tipoPessoa'] == 1)
-			    {
-			    	$pf = recuperaDados("pessoa_fisica","idPf",$projeto['idPf']);
-			    	$projeto['proponente'] = $pf['nome'];
-			    }
-			    else
-			    {
-			    	$pj = recuperaDados("pessoa_juridica","idPj",$projeto['idPj']);
-			    	$projeto['proponente'] = $pj['razaoSocial'];
-			    }
-			    $projeto['nomeProjeto'] = $projeto['nomeProjeto'];
-			    $projeto['protocolo'] = $projeto['protocolo'];
-			    $projeto['valorAprovado'] = $projeto['valorAprovado'];
-			    $projeto['renuncia'] = $renuncia['renunciaFiscal'];
-
-			    echo "<p><strong>Nome do projeto:</strong> ".$projeto['proponente']."</p>";
-			    echo "<p><strong>Protocolo:</strong> ".$projeto['protocolo']."</p>";
-			    echo "<p><strong>Valor total aprovado:</strong> ".$projeto['valorAprovado']."</p>";
-			    echo "<p><strong>Percentual de renúncia:</strong> ".$projeto['renuncia']."</p>";
-			    echo "<p>&nbsp;</p>";
-			}
-			
-		    $i++;
-		}
-		?>
+            foreach ($query as $projeto)
+            {
+                echo "<p><strong>Nome do projeto:</strong> ".$projeto['nomeProjeto']."</p>";
+                echo "<p><strong>Protocolo:</strong> ".$projeto['protocolo']."</p>";
+                echo "<p><strong>Valor total aprovado:</strong> ".$projeto['valorAprovado']."</p>";
+                echo "<p><strong>Percentual de renúncia:</strong> ".$projeto['renunciaFiscal']."</p>";
+                echo "<p>&nbsp;</p>";
+            }
+        }
+        ?>
 
 
 		<p>&nbsp;</p>
 		<p>&nbsp;</p>
-		<p><center>São Paulo, __ de ____ de 2018.</center></p>
+		<p><div style="text-align: center;">São Paulo, __ de ____ de 2018.</div></p>
 		<p>&nbsp;</p>
 		<p>&nbsp;</p>
 </body>
