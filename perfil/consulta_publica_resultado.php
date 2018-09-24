e1866b5a39fe4c5c077fe581da88c1c1090de583
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-09-24 15:27:56 -0300

    Consulta pública - Arrumar a busca para pesquisar qualquer projeto que esteja aprovado incluindo certificados

--- perfil/consulta_publica_resultado.php
+++ perfil/consulta_publica_resultado.php
@@ -8,7 +8,6 @@ $cnpj = $_POST['cnpj'];
 $nomeProjeto = $_POST['nomeProjeto'];
 $protocolo = $_POST['protocolo'];
 $idAreaAtuacao = $_POST['idAreaAtuacao'];
-$idStatus = $_POST['idStatus'];
 $valorAprovado = $_POST['valorAprovado'];
 
 // Inicio Pessoa Física
@@ -110,159 +109,6 @@ elseif($razaoSocial != '' || $cnpj != '')
 		$x['num'] = 0;
 	}
 }
-//filtro por status aprovado
-elseif ($idStatus == 5)
-{
-	if($idStatus == 5)
-	{
-		$filtro_idStatus = " AND idStatus = '$idStatus'";
-	}
-	else
-	{
-		$filtro_idStatus = "";
-	}
-
-	$sql = "SELECT * FROM projeto AS prj
-			WHERE publicado = 1 AND idStatus = 5
-			$filtro_idStatus";
-	$query = mysqli_query($con,$sql);
-	$num = mysqli_num_rows($query);
-	if($num > 0)
-	{
-		$i = 0;
-		while($lista = mysqli_fetch_array($query))
-		{
-			$area = recuperaDados("area_atuacao","idArea",$lista['idAreaAtuacao']);
-			$status = recuperaDados("status","idStatus",$lista['idStatus']);
-			$pf = recuperaDados("pessoa_fisica","idPf",$lista['idPf']);
-			$pj = recuperaDados("pessoa_juridica","idPj",$lista['idPj']);
-			$x[$i]['idProjeto'] = $lista['idProjeto'];
-			$x[$i]['protocolo'] = $lista['protocolo'];
-			$x[$i]['nomeProjeto'] = $lista['nomeProjeto'];
-			if($lista['tipoPessoa'] == 1)
-			{
-				$x[$i]['proponente'] = $pf['nome'];
-				$x[$i]['documento'] = $pf['cpf'];
-			}
-			else
-			{
-				$x[$i]['proponente'] = $pj['razaoSocial'];
-				$x[$i]['documento'] = $pj['cnpj'];
-			}
-			$x[$i]['areaAtuacao'] = $area['areaAtuacao'];
-			$x[$i]['status'] = $status['status'];
-			$i++;
-		}
-		$x['num'] = $i;
-	}
-	else
-	{
-		$x['num'] = 0;
-	}
-}
-
-//filtro por status reprovado
-elseif ($idStatus == 6)
-{
-	if($idStatus == 6)
-	{
-		$filtro_idStatus = " AND idStatus = '$idStatus'";
-	}
-	else
-	{
-		$filtro_idStatus = "";
-	}
-
-	$sql = "SELECT * FROM projeto AS prj
-			WHERE publicado = 1 AND idStatus = 6
-			$filtro_idStatus";
-	$query = mysqli_query($con,$sql);
-	$num = mysqli_num_rows($query);
-	if($num > 0)
-	{
-		$i = 0;
-		while($lista = mysqli_fetch_array($query))
-		{
-			$area = recuperaDados("area_atuacao","idArea",$lista['idAreaAtuacao']);
-			$status = recuperaDados("status","idStatus",$lista['idStatus']);
-			$pf = recuperaDados("pessoa_fisica","idPf",$lista['idPf']);
-			$pj = recuperaDados("pessoa_juridica","idPj",$lista['idPj']);
-			$x[$i]['idProjeto'] = $lista['idProjeto'];
-			$x[$i]['protocolo'] = $lista['protocolo'];
-			$x[$i]['nomeProjeto'] = $lista['nomeProjeto'];
-			if($lista['tipoPessoa'] == 1)
-			{
-				$x[$i]['proponente'] = $pf['nome'];
-				$x[$i]['documento'] = $pf['cpf'];
-			}
-			else
-			{
-				$x[$i]['proponente'] = $pj['razaoSocial'];
-				$x[$i]['documento'] = $pj['cnpj'];
-			}
-			$x[$i]['areaAtuacao'] = $area['areaAtuacao'];
-			$x[$i]['status'] = $status['status'];
-			$i++;
-		}
-		$x['num'] = $i;
-	}
-	else
-	{
-		$x['num'] = 0;
-	}
-}
-
-//filtro por status inscrito (comissão)
-elseif ($idStatus == 7)
-{
-	if($idStatus == 7)
-	{
-		$filtro_idStatus = " AND idStatus = '$idStatus'";
-	}
-	else
-	{
-		$filtro_idStatus = "";
-	}
-
-	$sql = "SELECT * FROM projeto AS prj
-			WHERE publicado = 1 AND idStatus = 7
-			$filtro_idStatus";
-	$query = mysqli_query($con,$sql);
-	$num = mysqli_num_rows($query);
-	if($num > 0)
-	{
-		$i = 0;
-		while($lista = mysqli_fetch_array($query))
-		{
-			$area = recuperaDados("area_atuacao","idArea",$lista['idAreaAtuacao']);
-			$status = recuperaDados("status","idStatus",$lista['idStatus']);
-			$pf = recuperaDados("pessoa_fisica","idPf",$lista['idPf']);
-			$pj = recuperaDados("pessoa_juridica","idPj",$lista['idPj']);
-			$x[$i]['idProjeto'] = $lista['idProjeto'];
-			$x[$i]['protocolo'] = $lista['protocolo'];
-			$x[$i]['nomeProjeto'] = $lista['nomeProjeto'];
-			if($lista['tipoPessoa'] == 1)
-			{
-				$x[$i]['proponente'] = $pf['nome'];
-				$x[$i]['documento'] = $pf['cpf'];
-			}
-			else
-			{
-				$x[$i]['proponente'] = $pj['razaoSocial'];
-				$x[$i]['documento'] = $pj['cnpj'];
-			}
-			$x[$i]['areaAtuacao'] = $area['areaAtuacao'];
-			$x[$i]['status'] = $status['status'];
-			$i++;
-		}
-		$x['num'] = $i;
-	}
-	else
-	{
-		$x['num'] = 0;
-	}
-}
-
 //Início Projeto
 else
 {
@@ -304,7 +150,7 @@ else
 
 
 	$sql = "SELECT * FROM projeto AS prj
-			WHERE publicado = 1 AND idStatus = 5
+			WHERE publicado = 1 AND idStatus IN (5, 21, 26, 16, 11)
 			$filtro_nomeProjeto $filtro_protocolo $filtro_idAreaAtuacao $filtro_valorAprovado";
 	$query = mysqli_query($con,$sql);
 	$num = mysqli_num_rows($query);
@@ -381,7 +227,7 @@ $mensagem = "Foram encontrados ".$x['num']." resultados";
 								echo "<td class='list_description'>".$x[$h]['proponente']."</td>";
 								echo "<td class='list_description'>".$x[$h]['documento']."</td>";
 								echo "<td class='list_description'>".$x[$h]['areaAtuacao']."</td>";
-								echo "<td class='list_description'>".$x[$h]['status']."</td>";
+								echo "<td class='list_description'>Aprovado</td>";
 								echo "<td class='list_description'>
 										<form method='POST' action='?perfil=consulta_publica_detalhes'>
 											<input type='hidden' name='consulta' value='1'>
