8efc98f251226e2ae8bb41e85130f779ae9706f4
Lorelei Gabriele <loreleigab@hotmail.com>
2018-09-28 20:58:46 -0300

    Remoção do código do rodapé para usuários diferentes do id de pf  =1

--- visual/rodape.php
+++ visual/rodape.php
@@ -19,12 +19,13 @@
 	<div class="container">
 		<div class="col-md-12">
 			<?php
-			if($_SESSION['idUser'] == 1 AND $_SESSION['tipoPessoa'] == 1)
-				echo "<strong>SESSION</strong><pre>", var_dump($_SESSION), "</pre>";
-				echo "<strong>POST</strong><pre>", var_dump($_POST), "</pre>";
-				echo "<strong>GET</strong><pre>", var_dump($_GET), "</pre>";
-				echo "<strong>FILES</strong><pre>", var_dump($_FILES), "</pre>";
-				echo ini_get('session.gc_maxlifetime')/60; // em minutos
+			if($_SESSION['idUser'] == 1 AND $_SESSION['tipoPessoa'] == 1) {
+                echo "<strong>SESSION</strong><pre>", var_dump($_SESSION), "</pre>";
+                echo "<strong>POST</strong><pre>", var_dump($_POST), "</pre>";
+                echo "<strong>GET</strong><pre>", var_dump($_GET), "</pre>";
+                echo "<strong>FILES</strong><pre>", var_dump($_FILES), "</pre>";
+                echo ini_get('session.gc_maxlifetime') / 60; // em minutos
+            }
 			?>
 		</div>
 	</div>
