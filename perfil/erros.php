67be020221842790a98888a3156f88964b39fea3
Diego Vicente <diego.vdossantos@gmail.com>
2018-10-15 19:49:11 -0300

    correção redirecionamento cpf invalido ficha tecnica

--- perfil/erros.php
+++ perfil/erros.php
@@ -29,6 +29,7 @@ switch($p)
 	<?php
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=representante_pj'>";
 	break;
+
 	case 'erro_cooperativa':
 	?>
 		<section id="list_items" class="home-section bg-white">
@@ -47,14 +48,15 @@ switch($p)
 <?php
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=projeto_novo'>";
 	break;
-	case 'erro_login_projeto':
+
+	case 'erro_ficha_tecnica':
 	?>
 		<section id="list_items" class="home-section bg-white">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-offset-2 col-md-8">
 						<div class="section-heading">
-							<h4><font color='red'>CNPJ inválido! por favor, insira o número correto!</font></h4>
+							<h4><font color='red'>CPF inválido! por favor, insira o número correto!</font></h4>
 							<h4><font color='red'>Redirecionando...</font></h4>
 							<p></p>
 						</div>
@@ -65,6 +67,7 @@ switch($p)
 <?php
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=ficha_tecnica'>";
 	break;
+
 	case 'erro_incentivador_representante1':
 ?>
 		<section id="list_items" class="home-section bg-white">
@@ -80,7 +83,7 @@ switch($p)
 				</div>
 			</div>
 		</section>
-	<?php
+<?php
 		echo "<meta HTTP-EQUIV='refresh' CONTENT='3.5;URL=?perfil=incentivador_representante_pj'>";
 	break;
 } //fim da switch
