a0542ac9ab3d521911309993477bf091f1ab6c57
LoreleiGab <loreleigab@hotmail.com>
2018-10-24 10:16:55 -0300

    Informação de encerramento de projetos e contato do rodapé

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ session_start();
 $con = bancoMysqli();
 $tipoPessoa = '6';
 ?>
-
+<script>
+    alert(" ATENÇÃO!\n" +
+        "\n" +
+        "As  inscrições de projetos se encerram no dia 31 de outubro, não deixem para última hora. ");
+</script>
 
 <html>
 	<head>
@@ -207,7 +211,7 @@ $tipoPessoa = '6';
 					<tr>
 						<td width="20%"><img src="visual/images/logo_promac_branco.png" align="left"/></td>
 						<td align="center"><font color="#ccc">2018 @ Pro-Mac - Programa Municipal de Apoio a Projetos Culturais<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
-						<td align="right"><font color="#ccc"><b>Contato:</b><br/><i class="fa fa-phone-square"></i> (11) 3397-0063<br/><i class="fa fa-phone-square"></i> (11) 3397-0228<br/><i class="fa fa-envelope"></i> promac@prefeitura.sp.gov.br</font></td>
+						<td align="right"><font color="#ccc"><b>Contato:</b><br/><i class="fa fa-phone-square"></i> (11) 3397-0002 das 13h às 17h<br/><i class="fa fa-envelope"></i> Dúvidas: promac@prefeitura.sp.gov.br</font></td>
 					</tr>
 				</table>
 			</div>
--- visual/rodape_index.php
+++ visual/rodape_index.php
@@ -5,7 +5,7 @@
 			<tr>
 				<td width="20%"><img src="visual/images/logo_promac_branco.png" align="left"/></td>
 				<td align="center"><font color="#ccc">2018 @ Pro-Mac - Programa Municipal de Apoio a Projetos Culturais<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
-				<td align="right"><font color="#ccc"><b>Contato:</b><br/><i class="fa fa-phone-square"></i> (11) 3397-0063<br/><i class="fa fa-phone-square"></i> (11) 3397-0228<br/><i class="fa fa-envelope"></i> promac@prefeitura.sp.gov.br</font></td>
+				<td align="right"><font color="#ccc"><b>Contato:</b><br/><i class="fa fa-phone-square"></i> (11) 3397-0002 das 13h às 17h<br/><i class="fa fa-envelope"></i> Dúvidas: promac@prefeitura.sp.gov.br</font></td>
 			</tr>
 		</table>
 	</div>
