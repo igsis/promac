fc076c79c2f00b5141b835662f8fa9c1ffa403fe
Isaías Mendes <isaiaszmendes@gmail.com>
2018-11-27 17:18:46 -0200

    cronograma funcionando, validações de campos

--- perfil/cronograma.php
+++ perfil/cronograma.php
@@ -38,28 +38,34 @@ if(isset($_POST['insereCronograma']))
 	$posProducao = $_POST['posProducao'];
 	$prestacaoContas = $_POST['prestacaoContas'];
 
-	$sql_insere_cronograma = "INSERT INTO `cronograma`(captacaoRecurso, preProducao, producao, posProducao, prestacaoContas) VALUES ('$captacaoRecurso', '$preProducao', '$producao', '$posProducao', '$prestacaoContas')";
 
-	if(mysqli_query($con,$sql_insere_cronograma))
-	{
-		$sql_ultimo = "SELECT idCronograma FROM cronograma ORDER BY idCronograma DESC LIMIT 0,1";
-		$query_ultimo = mysqli_query($con,$sql_ultimo);
-		$ultimoCronograma = mysqli_fetch_array($query_ultimo);
-		$idCronograma = $ultimoCronograma['idCronograma'];
-		$sql_insere_cronograma_evento = "UPDATE projeto SET idCronograma = '$idCronograma' WHERE idProjeto = '$idProjeto'";
-		if(mysqli_query($con,$sql_insere_cronograma_evento))
+	if($captacaoRecurso != null  && $preProducao != null &&  $producao != null && $posProducao != null && $prestacaoContas != null){
+
+		$sql_insere_cronograma = "INSERT INTO `cronograma`(captacaoRecurso, preProducao, producao, posProducao, prestacaoContas) VALUES ('$captacaoRecurso', '$preProducao', '$producao', '$posProducao', '$prestacaoContas')";
+
+		if(mysqli_query($con,$sql_insere_cronograma))
 		{
-			$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
-			gravarLog($sql_insere_cronograma_evento);
+			$sql_ultimo = "SELECT idCronograma FROM cronograma ORDER BY idCronograma DESC LIMIT 0,1";
+			$query_ultimo = mysqli_query($con,$sql_ultimo);
+			$ultimoCronograma = mysqli_fetch_array($query_ultimo);
+			$idCronograma = $ultimoCronograma['idCronograma'];
+			$sql_insere_cronograma_evento = "UPDATE projeto SET idCronograma = '$idCronograma' WHERE idProjeto = '$idProjeto'";
+			if(mysqli_query($con,$sql_insere_cronograma_evento))
+			{
+				$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso!</strong></font>";
+				gravarLog($sql_insere_cronograma_evento);
+			}
+			else
+			{
+				$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+			}
 		}
 		else
 		{
 			$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
 		}
-	}
-	else
-	{
-		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	}else{
+		$mensagem = "<font color='#FF0000'><strong>Necessário preencher todos as etapas!</strong></font>";
 	}
 }
 
@@ -75,23 +81,27 @@ if(isset($_POST['editaCronograma']))
 	$posProducao = $_POST['posProducao'];
 	$prestacaoContas = $_POST['prestacaoContas'];
 
-	$sql_edita_cronograma = "UPDATE `cronograma` SET
-	captacaoRecurso = '$captacaoRecurso',
-	preProducao = '$preProducao',
-	producao = '$producao',
-	posProducao = '$posProducao',
-	prestacaoContas = '$prestacaoContas',
-	alteradoPor = '$usuarioLogado'
-	WHERE idCronograma = '$idCronograma'";
-	if(mysqli_query($con,$sql_edita_cronograma))
-	{
-		$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso! Utilize o menu para avançar.</strong></font>";
-        gravarLog($sql_edita_cronograma);
-        echo "<meta HTTP-EQUIV='refresh' CONTENT='0.5;URL=?perfil=cronograma'>";
-	}
-	else
-	{
-		$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+	if($captacaoRecurso != null  && $preProducao != null &&  $producao != null && $posProducao != null && $prestacaoContas != null){
+		$sql_edita_cronograma = "UPDATE `cronograma` SET
+		captacaoRecurso = '$captacaoRecurso',
+		preProducao = '$preProducao',
+		producao = '$producao',
+		posProducao = '$posProducao',
+		prestacaoContas = '$prestacaoContas',
+		alteradoPor = '$usuarioLogado'
+		WHERE idCronograma = '$idCronograma'";
+		if(mysqli_query($con,$sql_edita_cronograma))
+		{
+			$mensagem = "<font color='#01DF3A'><strong>Gravado com sucesso! Utilize o menu para avançar.</strong></font>";
+			gravarLog($sql_edita_cronograma);
+			echo "<meta HTTP-EQUIV='refresh' CONTENT='0.9;URL=?perfil=cronograma'>";
+		}
+		else
+		{
+			$mensagem = "<font color='#FF0000'><strong>Erro ao gravar! Tente novamente.</strong></font>";
+		}
+	}else{
+		$mensagem = "<font color='#FF0000'><strong>Necessário preencher todos as etapas!</strong></font>";
 	}
 }
 
@@ -145,12 +155,14 @@ if(isset($_POST['editaCronograma']))
 					<div class="col-md-offset-2 col-md-8"><br/></div>
 				</div>
 
-				<div class="form-group">
-					<h4>Cronograma</h4>
-				</div>
+	
 				<div class="row">
 					<div class=" col-md-12">
+
 						<form method="POST" action="?perfil=cronograma" class="form-horizontal" role="form">
+							<div class="form-group">
+								<h4>Etapas</h4>
+							</div>
 
 							<div class="form-group">
 								<div class="col-md-offset-2 col-md-6">
@@ -278,95 +290,20 @@ if(isset($_POST['editaCronograma']))
 
 							<div class="form-group">
 								<div class="col-md-offset-2 col-md-8">
-									<input type="button" name="insereCronograma" class="btn btn-theme btn-lg btn-block" value="Inserir">
+									<?php 
+										if($idCronograma){
+											echo "<input type='submit' name='editaCronograma' class='btn btn-theme btn-lg btn-block' value='Inserir'>";
+										}else{
+											echo "<input type='submit' name='insereCronograma' class='btn btn-theme btn-lg btn-block' value='Inserir'>";
+										}
+									?>
+									
 								</div>
 							</div>
 						</form>
 
 					</div>
-				</div>
-				<!-- Inserir script aqui  -->
-				<script>
-
-					let btnsMenos = document.querySelectorAll('.menos') // pega todos os buttons .menos
-					let btnsMais = document.querySelectorAll('.mais') // pega todos os buttons .mais
-					let captacaoRecurso = document.querySelector('#captacaoRecurso')					
-					let etapas = document.querySelectorAll('.progress input')
-					let btnInserir = document.querySelector('input[name="insereCronograma"]')
-					
-
-					const quantidadeMes = (val) => {
-					
-						if((val / 6.25) == 1) // meio
-						{ 
-							return `Metade de um mês`
-						}
-						else if((val / 6.25) == 2) // um 
-						{ 
-							return `${(val / 12.5)} Mês`
-						}
-						else if((val / 6.25) == 3) 	// um e meio
-						{ 
-							return `${parseInt(val / 12.5)} Mês e Meio`
-						}
-						else if((val / 6.25) % 2 == 0) 	// par meses
-						{ 
-							return `${(val / 12.5)} Meses`
-						}
-						else{				// meses e meio
-							return `${parseInt(val / 12.5)} Meses e Meio`
-						}
-					}
-
-					const preencher = (item) => {
-						let val = parseFloat(item.value)
-						if(!isNaN(val)){
-							elemento = item.parentNode.children[0]
-							elemento.style.width = ((val / .5) * 6.25) + `%`
-							elemento.innerHTML = quantidadeMes(parseFloat((val / .5) * 6.25)) // exibe qtd de meses
-						}
-					}					
-					
-					for(let etapa of etapas){
-						veliricador.push(isNaN(parseFloat(etapa.value))) 
-						preencher(etapa)
-					}					
-					
-					const menos = (barra) => {
-						let val = barra.style.width.replace('%','')
-						if(val > '0'){
-							val = (parseFloat(val) - parseFloat('6.25%'))
-							barra.style.width = `${val}%`
-							barra.innerHTML = quantidadeMes(val)
-							barra.parentNode.children[1].value = (val / 12.50) // insere no value do input						
-						}
-					}
-
-					const mais = (barra) => {
-						let val = barra.style.width.replace('%','')
-						if(val < parseFloat('100')){
-							val = (parseFloat(val) + parseFloat('6.25%'))
-							barra.style.width = `${val}%`
-							barra.innerHTML = quantidadeMes(val)
-							barra.parentNode.children[1].value = (val / 12.50) // insere no value do input							
-						}
-					}				
-					
-					for(let btn of btnsMenos){
-						btn.addEventListener('click', () => {
-							let barra = btn.parentNode.parentNode.children[1].querySelector('.progress .progress-bar')			
-							menos(barra)
-						})
-					}
-					
-					for(let btn of btnsMais){
-						btn.addEventListener('click', () => {		
-							let barra = btn.parentNode.parentNode.children[1].querySelector('.progress .progress-bar')								
-							mais(barra)
-						})
-					}
-
-				</script>
+				</div>		
 
 				<div class="table-responsive list_info">
 					<table class='table table-condensed'>
@@ -382,23 +319,23 @@ if(isset($_POST['editaCronograma']))
 							</tr>
 							<tr>
 								<td class='list_description'>Captação de recursos</td>
-								<td class='list_description'><?= $cronograma['captacaoRecurso'] ?? '' ?></td>
+								<td class='list_description exibir'><?= $cronograma['captacaoRecurso'] ?? '' ?></td>
 							</tr>
 							<tr>
 								<td class='list_description'>Pré-Produção</td>
-								<td class='list_description'><?= $cronograma['preProducao'] ?? '' ?></td>
+								<td class='list_description exibir'><?= $cronograma['preProducao'] ?? '' ?></td>
 							</tr>
 							<tr>
 								<td class='list_description'>Produção</td>
-								<td class='list_description'><?= $cronograma['producao'] ?? '' ?></td>
+								<td class='list_description exibir'><?= $cronograma['producao'] ?? '' ?></td>
 							</tr>
 							<tr>
 								<td class='list_description'>Pós-Produção</td>
-								<td class='list_description'><?= $cronograma['posProducao'] ?? '' ?></td>
+								<td class='list_description exibir'><?= $cronograma['posProducao'] ?? '' ?></td>
 							</tr>
 							<tr>
 								<td class='list_description'>Prestação de Contas</td>
-								<td class='list_description'><?= $cronograma['prestacaoContas'] ?? '' ?></td>
+								<td class='list_description exibir'><?= $cronograma['prestacaoContas'] ?? '' ?></td>
 							</tr>
 						</tbody>
 					</table>
@@ -407,3 +344,90 @@ if(isset($_POST['editaCronograma']))
 		</div>
 	</div>
 </section>
+
+<script>
+
+	let btnsMenos = document.querySelectorAll('.menos') // pega todos os buttons .menos
+	let btnsMais = document.querySelectorAll('.mais') // pega todos os buttons .mais
+	let captacaoRecurso = document.querySelector('#captacaoRecurso')					
+	let etapas = document.querySelectorAll('.progress input')
+	let btnInserir = document.querySelector('input[name="insereCronograma"]')
+	let listaEtapas = document.querySelectorAll('.exibir') // lista
+
+
+	const quantidadeMes = (val) => {
+
+		if((val / 6.25) == 1) // meio
+		{ 
+			return `Metade de um mês`
+		}
+		else if((val / 6.25) == 2) // um 
+		{ 
+			return `${(val / 12.5)} Mês`
+		}
+		else if((val / 6.25) == 3) 	// um e meio
+		{ 
+			return `${parseInt(val / 12.5)} Mês e Meio`
+		}
+		else if((val / 6.25) % 2 == 0) 	// par meses
+		{ 
+			return `${(val / 12.5)} Meses`
+		}
+		else{				// meses e meio
+			return `${parseInt(val / 12.5)} Meses e Meio`
+		}
+	}
+
+	const preencher = (item) => {
+		let val = parseFloat(item.value)
+		if(!isNaN(val)){
+			elemento = item.parentNode.children[0]
+			elemento.style.width = ((val / .5) * 6.25) + `%`
+			elemento.innerHTML = quantidadeMes(parseFloat((val / .5) * 6.25)) // exibe qtd de meses
+		}
+	}					
+
+	for(let etapa of etapas){
+		
+		preencher(etapa)
+	}				
+
+	const menos = (barra) => {
+		let val = barra.style.width.replace('%','')
+		if(val > '0'){
+			val = (parseFloat(val) - parseFloat('6.25%'))
+			barra.style.width = `${val}%`
+			barra.innerHTML = quantidadeMes(val)
+			barra.parentNode.children[1].value = (val / 12.50) // insere no value do input						
+		}
+	}
+
+	const mais = (barra) => {
+		let val = barra.style.width.replace('%','')
+		if(val < parseFloat('100')){
+			val = (parseFloat(val) + parseFloat('6.25%'))
+			barra.style.width = `${val}%`
+			barra.innerHTML = quantidadeMes(val)
+			barra.parentNode.children[1].value = (val / 12.50) // insere no value do input							
+		}
+	}				
+
+	for(let btn of btnsMenos){
+		btn.addEventListener('click', () => {
+			let barra = btn.parentNode.parentNode.children[1].querySelector('.progress .progress-bar')			
+			menos(barra)
+		})
+	}
+
+	for(let btn of btnsMais){
+		btn.addEventListener('click', () => {		
+			let barra = btn.parentNode.parentNode.children[1].querySelector('.progress .progress-bar')								
+			mais(barra)
+		})
+	}
+
+	for(let etapa of listaEtapas){	
+		etapa.innerHTML = quantidadeMes(parseFloat((etapa.innerHTML / .5) * 6.25))
+	}
+
+</script>
