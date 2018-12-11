cc0509bc2e2da78e22f21415645490c3afefa647
Diego Vicente <diego.vdossantos@gmail.com>
2018-12-11 17:52:10 -0200

    Comandos SQL utilizados na atualização do banco de dados

--- #modelagem/promac_atualização_10-12-2018.sql
+++ #modelagem/promac_atualização_10-12-2018.sql
@@ -1,10 +1,12 @@
 USE `promac`;
 
-ALTER TABLE projeto ADD dataParecerista DATE NOT NULL AFTER idStatusParecerista
+ALTER TABLE `projeto` ADD `dataParecerista` DATE NOT NULL AFTER `idStatusParecerista`;
 
 CREATE TABLE IF NOT EXISTS `promac`.`agendamento` (
  `id` INT NOT NULL AUTO_INCREMENT,
-  `linkAgendamento` VARCHAR(45) NOT NULL,
+  `linkAgendamento` VARCHAR(150) NOT NULL,
   `data` DATE NULL,
   PRIMARY KEY (`id`)
 );
+
+ALTER TABLE `historico_reuniao` CHANGE `idStatus` `idEtapaProjeto` INT(11) NOT NULL;
