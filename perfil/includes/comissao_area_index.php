62584dd4ab572479c47161ae23d7f0725b3ccb12
Diego Vicente <diego.vdossantos@gmail.com>
2019-02-22 13:15:43 -0300

    #278 - Presidente comissão visualizar listas de projetos

--- perfil/includes/comissao_area_index.php
+++ perfil/includes/comissao_area_index.php
@@ -8,6 +8,23 @@
     {
         $parecerista = " AND idComissao = $idPf";
     }
+    $etapas = [
+            "Novos e com Solicitação de Alteração" => "7, 34",
+            "com Complemento e Recurso" => "19, 24"];
+
+$sqlProjetoVerificado = "SELECT idProjeto, nomeProjeto, protocolo,  idComissao, pro.envioComissao, pro.dataParecerista, pro.idStatus, pf.nome, pf.cpf, razaoSocial, cnpj, areaAtuacao, pro.publicado, pfc.nome AS comissao, etapaProjeto, pro.idEtapaProjeto AS idEtapaProjeto 
+                    FROM projeto AS pro
+                    LEFT JOIN pessoa_fisica AS pf ON pro.idPf = pf.idPf
+                    LEFT JOIN pessoa_juridica AS pj ON pro.idPj = pj.idPj
+                    INNER JOIN area_atuacao AS ar ON pro.idAreaAtuacao = ar.idArea
+                    LEFT JOIN pessoa_fisica AS pfc ON pro.idComissao = pfc.idPf 
+                    INNER JOIN etapa_projeto AS etapa ON pro.idEtapaProjeto = etapa.idEtapaProjeto
+                    WHERE pro.idStatus != 6 AND pro.idEtapaProjeto IN (".implode(", ", $etapas).") $parecerista AND pro.verificadoComissao = '1'
+                    ORDER BY pro.envioComissao ASC";
+$queryProjetoVerificado = mysqli_query($con, $sqlProjetoVerificado);
+$numVerificado = mysqli_num_rows($queryProjetoVerificado);
+
+foreach ($etapas as $texto => $etapa) {
     $sqlProjeto = "SELECT idProjeto, nomeProjeto, protocolo,  idComissao, pro.envioComissao, pro.dataParecerista, pro.idStatus, pf.nome, pf.cpf, razaoSocial, cnpj, areaAtuacao, pro.publicado, pfc.nome AS comissao, etapaProjeto, pro.idEtapaProjeto AS idEtapaProjeto 
                     FROM projeto AS pro
                     LEFT JOIN pessoa_fisica AS pf ON pro.idPf = pf.idPf
@@ -15,86 +32,151 @@
                     INNER JOIN area_atuacao AS ar ON pro.idAreaAtuacao = ar.idArea
                     LEFT JOIN pessoa_fisica AS pfc ON pro.idComissao = pfc.idPf 
                     INNER JOIN etapa_projeto AS etapa ON pro.idEtapaProjeto = etapa.idEtapaProjeto
-                    WHERE pro.idStatus != 6 AND pro.idEtapaProjeto IN (7, 19, 24, 34) $parecerista AND pro.verificadoComissao = '0'
+                    WHERE pro.idStatus != 6 AND pro.idEtapaProjeto IN ($etapa) $parecerista AND pro.verificadoComissao = '0'
                     ORDER BY pro.envioComissao ASC";
-    $queryProjeto = mysqli_query($con,$sqlProjeto);
+    $queryProjeto = mysqli_query($con, $sqlProjeto);
     $num = mysqli_num_rows($queryProjeto);
 ?>
-        <div class="row">
-            <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
 
-                <div class='form-group'>
-                    <h5>Projetos Encaminhados à Comissão
-                    <small>Máximo de 10 registros exibidos</small></h5>
-                    <span class="label label-warning">Total: <?=$num?></span>
-                </div>
+            <div class='form-group'>
+                <h5>Projetos <?= $texto ?> Encaminhados a Comissão <br>
+                <small>Máximo de 10 registros exibidos</small></h5>
+                <span class="label label-warning">Total: <?=$num?></span>
+            </div>
 
-                <div class="table-responsive list_info">
+            <div class="table-responsive list_info">
+                <?php
+                if($num > 0)
+                {
+                ?>
+
+                <table class='table table-condensed'>
+                    <thead>
+                    <tr class='list_menu'>
+                        <td>Encaminhado dia</td>
+                        <td>Protocolo (nº ISP)</td>
+                        <td>Nome do Projeto</td>
+                        <td>Proponente</td>
+                        <td>Documento</td>
+                        <td>Área de Atuação</td>
+                        <td>Parecerista</td>
+                        <td>Projetos com Etapa</td>
+                        <td>Ação</td>
+                    </tr>
+                    </thead>
                     <?php
-                    if($num > 0)
+                    $i = 0;
+                    while ($campo = mysqli_fetch_array($queryProjeto))
                     {
-                    ?>
-
-                    <table class='table table-condensed'>
-                        <thead>
-                        <tr class='list_menu'>
-                            <td>Encaminhado dia</td>
-                            <td>Protocolo (nº ISP)</td>
-                            <td>Nome do Projeto</td>
-                            <td>Proponente</td>
-                            <td>Documento</td>
-                            <td>Área de Atuação</td>
-                            <td>Parecerista</td>
-                            <td>Projetos com Etapa</td>
-                            <td>Ação</td>
-                        </tr>
-                        </thead>
-                        <?php
-                        $i = 0;
-                        while ($campo = mysqli_fetch_array($queryProjeto))
+                        if ($i < 10)
                         {
-                            if ($i < 10)
-                            {
-                                $idComissao = $campo ['idComissao'];
-                                $idProjeto = $campo['idProjeto'];
+                            $idComissao = $campo ['idComissao'];
+                            $idProjeto = $campo['idProjeto'];
 
-                                $dataEnvio = new DateTime ($campo['envioComissao']);
-                                ?>
-                                <tr>
-                                    <td class='list_description'><?= date_format($dataEnvio, "d/m/Y") ?></td>
-                                    <td class='list_description maskProtocolo' data-mask = "0000.00.00/0000000"><?= $campo['protocolo'] ?></td>
-                                    <td class='list_description'><?= $campo['nomeProjeto'] ?></td>
-                                    <td class='list_description'><?= isset($campo['nome']) ? $campo['nome'] : $campo['razaoSocial'] ?></td>
-                                    <td class='list_description'><?= isset($campo['cpf']) ? $campo['cpf'] : $campo['cnpj'] ?></td>
-                                    <td class='list_description'><?= mb_strimwidth($campo['areaAtuacao'], 0, 38, "...") ?></td>
-                                    <td class='list_description'><?=$campo['comissao']?></td>
-                                    <td> <?= recuperaDados('etapa_projeto', 'idEtapaProjeto', $campo['idEtapaProjeto'])['etapaProjeto'] ?> </td>
+                            $dataEnvio = new DateTime ($campo['envioComissao']);
+                            ?>
+                            <tr>
+                                <td class='list_description'><?= date_format($dataEnvio, "d/m/Y") ?></td>
+                                <td class='list_description maskProtocolo' data-mask = "0000.00.00/0000000"><?= $campo['protocolo'] ?></td>
+                                <td class='list_description'><?= $campo['nomeProjeto'] ?></td>
+                                <td class='list_description'><?= isset($campo['nome']) ? $campo['nome'] : $campo['razaoSocial'] ?></td>
+                                <td class='list_description'><?= isset($campo['cpf']) ? $campo['cpf'] : $campo['cnpj'] ?></td>
+                                <td class='list_description'><?= mb_strimwidth($campo['areaAtuacao'], 0, 38, "...") ?></td>
+                                <td class='list_description'><?=$campo['comissao']?></td>
+                                <td> <?= recuperaDados('etapa_projeto', 'idEtapaProjeto', $campo['idEtapaProjeto'])['etapaProjeto'] ?> </td>
 
-                                <?php
-                                    if ($i == 0)
-                                    {
-                                        ?>
-                                        <td class='list_description'>
-                                            <form method='POST'
-                                                  action='<?= ($pf['idNivelAcesso'] == 2) ? "?perfil=smc_detalhes_projeto" : "?perfil=comissao_detalhes_projeto" ?>'>
-                                                <input type='hidden' name='idProjeto'
-                                                       value='<?= $campo['idProjeto'] ?>'/>
-                                                <input type='submit' class='btn btn-theme btn-block' value='Visualizar'>
-                                            </form>
-                                        </td>
-                                        <?php
-                                    }
-                                echo "</tr>";
-                                $i++;
-                            }
+                            <?php
+                                if ($i == 0)
+                                {
+                                    ?>
+                                    <td class='list_description'>
+                                        <form method='POST'
+                                              action='<?= ($pf['idNivelAcesso'] == 2) ? "?perfil=smc_detalhes_projeto" : "?perfil=comissao_detalhes_projeto" ?>'>
+                                            <input type='hidden' name='idProjeto'
+                                                   value='<?= $campo['idProjeto'] ?>'/>
+                                            <input type='submit' class='btn btn-theme btn-block' value='Visualizar'>
+                                        </form>
+                                    </td>
+                                    <?php
+                                }
+                            echo "</tr>";
+                            $i++;
                         }
                     }
-                    else
-                    {
-                        echo "Não há resultado no momento.";
-                    }
-                    ?>
-                    </table>
-                </div>
+                }
+                else
+                {
+                    echo "Não há resultado no momento.";
+                }
+                ?>
+                </table>
             </div>
         </div>
+    </div>
+<?php
+}
+?>
+<div class="row">
+    <div class="col-md-12">
+
+        <div class='form-group'>
+            <h5>Projetos já verificados e atribuídos à pareceristas <br>
+                <small>Máximo de 10 registros exibidos</small></h5>
+            <span class="label label-warning">Total: <?=$numVerificado?></span>
+        </div>
+
+        <div class="table-responsive list_info">
+            <?php
+            if($numVerificado > 0)
+            {
+            ?>
+
+            <table class='table table-condensed'>
+                <thead>
+                <tr class='list_menu'>
+                    <td>Protocolo (nº ISP)</td>
+                    <td>Nome do Projeto</td>
+                    <td>Proponente</td>
+                    <td>Documento</td>
+                    <td>Área de Atuação</td>
+                    <td>Parecerista</td>
+                    <td>Projetos com Etapa</td>
+                </tr>
+                </thead>
+                <?php
+                $i = 0;
+                while ($campo = mysqli_fetch_array($queryProjetoVerificado))
+                {
+                    if ($i < 10)
+                    {
+                    $idComissao = $campo ['idComissao'];
+                    $idProjeto = $campo['idProjeto'];
+
+                    ?>
+                    <tr>
+                        <td class='list_description maskProtocolo' data-mask = "0000.00.00/0000000"><?= $campo['protocolo'] ?></td>
+                        <td class='list_description'><?= $campo['nomeProjeto'] ?></td>
+                        <td class='list_description'><?= isset($campo['nome']) ? $campo['nome'] : $campo['razaoSocial'] ?></td>
+                        <td class='list_description'><?= isset($campo['cpf']) ? $campo['cpf'] : $campo['cnpj'] ?></td>
+                        <td class='list_description'><?= mb_strimwidth($campo['areaAtuacao'], 0, 38, "...") ?></td>
+                        <td class='list_description'><?=$campo['comissao']?></td>
+                        <td> <?= recuperaDados('etapa_projeto', 'idEtapaProjeto', $campo['idEtapaProjeto'])['etapaProjeto'] ?> </td>
+
+
+                        </tr>
+                    <?php
+                    $i++;
+                    }
+                }
+            }
+            else
+            {
+                echo "Não há resultado no momento.";
+            }
+            ?>
+            </table>
+        </div>
+    </div>
+</div>
