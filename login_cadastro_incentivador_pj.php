37caa0b46f9a0f7176a4c96dcf70057c535898ce
Lorelei Gabriele <loreleigab@hotmail.com>
2020-01-14 12:28:05 -0300

    Verificação de email em PHP no cadastro de incentivador PJ

--- login_cadastro_incentivador_pj.php
+++ login_cadastro_incentivador_pj.php
@@ -12,6 +12,12 @@ if(isset($_POST['cadastraNovoPj']))
 	{
 		$mensagem = "<font color='#FF0000'><strong>Por favor, preencha todos os campos.</strong></font>";
 	}
+        elseif($_POST['email']){
+                $verifica = $con->quero("SELECT email FROM incentivador_pessoa_juridica WHERE email = '$email'");
+                        if($verifica != NULL){
+                               $mensagem = "<font color='#FF0000'><strong>O e-mail informado existe em outro cadastro. Utilize outro e-mail para prosseguir.</strong></font>";
+                        }
+        }
 	else
 	{
 		//verifica se há um post
