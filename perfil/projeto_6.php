12a362d53b8f47eb0c91530c2ffef545a77bd2ed
Diego Vicente <diego.vdossantos@gmail.com>
2020-01-18 12:31:14 -0300

    Alteração 9 - Precisa de complementação

--- perfil/projeto_6.php
+++ perfil/projeto_6.php
@@ -48,7 +48,8 @@ $projeto = recuperaDados("projeto","idProjeto",$idProjeto);
 
 					<div class="form-group">
 						<div class="col-md-offset-2 col-md-8">
-							<label>Metodologia e parâmetros a serem utilizados para aferição do cumprimento de metas</label>
+							<label>Plano de Trabalho</label>
+                            <p align="justify">Aqui você deverá elencar de forma organizada as atividades a serem desenvolvidas para atingir cada objetivo específico elencado no item Objetivos a serem alcançados com o projeto, quanto tempo levará para executar cada uma delas e qual produto será entregue para confirmar a execução. O plano de trabalho ajuda você a se organizar quanto ao que deve fazer para realizar seu projeto e ajuda SMC a entender o que será realizado e entregue. O modelo de Plano de Trabalho se encontra no Anexo VII do Edital do PROMAC 2020</p>
 							<textarea name="metodologia" class="form-control" rows="10" required><?php echo $projeto['metodologia'] ?></textarea>
 						</div>
 					</div>
