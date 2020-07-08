e66cb4431b0d4ca705190bf4b8b48d9f67cb549b
LoreleiGab <loreleigab@hotmail.com>
2020-07-08 12:57:08 -0300

    Término da área que permite a mudança de status manualmente

--- perfil/includes/label_smc_status.php
+++ perfil/includes/label_smc_status.php
@@ -4,11 +4,10 @@
             <?php if(isset($mensagem)){echo $mensagem;}; ?>
         </h5>
 
-
         <div class="row">
             <div class="form-group">
                 <div class="col-md-offset-1 col-md-2">
-                    <label>ATUAL:</label>
+                    <h6>ATUAL:</h6>
                 </div>
                 <div class="col-md-4">
                     <label>Etapa:</label> <?= $etapa['etapaProjeto'] ?>
@@ -21,18 +20,25 @@
 
         <hr/>
 
-        <h6>Alterar para:</h6>
         <div class="form-group">
-            <form method="POST" id='cancelarProjeto' action="?perfil=smc_detalhes_projeto" class="form-horizontal" role="form">
-                <label>Etapa:</label>
-                <select class="form-control" name="idEtapa" required>
-                    <option value="">Selecione...</option>
-                    <?php echo geraOpcao("etapa_projeto","") ?>
-                </select>
-                <div class="modal-footer">
+            <div class="col-md-offset-1 col-md-3">
+                <h6>ALTERAR PARA:</h6>
+            </div>
+            <form method="POST" id='alteraStatus' action="?perfil=smc_detalhes_projeto" class="form-horizontal" role="form">
+                <div class="col-md-5">
+                    <select class="form-control" name="idEtapa" required>
+                        <option value="">Selecione uma etapa</option>
+                        <?php
+                        $query = $con->query("SELECT * FROM etapa_projeto ORDER BY ordem");
+                        while ($option = mysqli_fetch_row($query)) {
+                            echo "<option value='" . $option[0] . "'>" . $option[1] . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="col-md-2">
                     <input type='hidden' name='idProjeto' value='<?php echo $idProjeto ?>'>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Voltar</button>
-                    <button type='submit' class='btn btn-danger btn-sm' style="border-radius: 10px;" name="cancelarProjeto">Confirmar</button>
+                    <button type='submit' class='btn btn-danger btn-sm' style="border-radius: 10px;" name="alteraStatus">Confirmar</button>
                 </div>
             </form>
         </div>
