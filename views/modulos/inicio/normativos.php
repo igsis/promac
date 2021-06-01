b53cec3fc72eb331cc97d4458d11176bf182f4c9
Lorelei <loreleigab@hotmail.com>
2021-06-01 20:35:17 -0300

    Início Normativos

--- views/modulos/inicio/normativos.php
+++ views/modulos/inicio/normativos.php
@@ -1,79 +1,140 @@
-<?php
-if (isset($_SESSION)) {
-    unset($_SESSION['origem_id_c']);
-    unset($_SESSION['pedido_id_c']);
-    unset($_SESSION['modulo_c']);
-    unset($_SESSION['edital_c']);
-}
-require_once "./controllers/FomentoController.php";
-$fomentoObj = new FomentoController();
-$now = date('Y-m-d H:i:s');
-$fomentos = $fomentoObj->listaFomentos();
-?>
-<div class="background">
-    <div class="content-header bg-dark mb-5 elevation-5">
-        <div class="container-fluid">
-            <div class="row mb-2 align-items-center justify-content-around">
-                <div class="col-sm-10 bg-dark">
-                    <a href="<?= SERVERURL ?>" class="brand-link">
-                        <img src="<?= SERVERURL ?>views/dist/img/AdminLTELogo.png" alt="CAPAC Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                        <span class="brand-text font-weight-light"><?= NOMESIS ?> - Fomentos</span>
-                    </a>
-                </div>
-                <div class="col-sm-1 mr-5">
-                    <img src="<?= SERVERURL ?>views/dist/img/CULTURA_PB_NEGATIVO_HORIZONTAL.png" alt="logo cultura">
-                </div>
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    <div class="content">
-        <div class="container-fluid">
-            <?php if (count($fomentos)): ?>
-                <?php foreach ($fomentos as $fomento): ?>
-                    <div class="row">
-                        <div class="offset-1 col-10">
-                            <div class="card card-dark card-outline collapsed-card elevation-5">
-                                <div class="card-header" data-card-widget="collapse" style="cursor: pointer;">
-                                    <h3 class="card-title"><?= $fomento['titulo'] ?></h3>
-                                    <div class="card-tools">
-                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <?= nl2br($fomento['descricao']) ?>
-                                </div>
-                                <div class="card-footer">
-                                    <?php if ($fomento['data_abertura'] <= $now && $now <= $fomento['data_encerramento']): ?>
-                                        <a href="login&modulo=6&edital=<?= $fomentoObj->encryption($fomento['id']) ?>"
-                                           class="small-box-footer">
-                                            Inscreva-se <i class="fas fa-arrow-circle-right"></i>
-                                        </a>
-                                    <?php else: ?>
-                                        <b>Inscrição não disponível</b>";
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /.col-md-6 -->
-                    </div>
-                    <!-- /.row -->
-                <?php endforeach; ?>
-            <?php else: ?>
-                <div class="row">
-                    <div class="offset-1 col-10">
-                        <div class="card card-dark card-outline collapsed-card elevation-5">
-                            <div class="card-header" style="cursor: default;">
-                                <h3 class="card-title">Não existe edital aberto no momento</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.col-md-6 -->
-                </div>
-                <!-- /.row -->
-            <?php endif; ?>
-        </div>
-        <!-- /.container-fluid -->
-    </div>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-9">
+                <h1 class="m-0 text-dark">Normativos</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
 </div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Normativos Vigentes</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/Lei_ProMac.pdf" target="_blank">Lei nº 15.948/2013</a></li>
+                            <li class="list-group-item"><a href="pdf/Novo_Decreto_ProMac.pdf" target="_blank">Decreto nº 59.119/2019</a></li>
+                            <li class="list-group-item"><a href="pdf/Retificação_Art31_Decreto.pdf" target="_blank">Retificação do inciso III do Art. 31 do Decreto nº 59.119/2019</a></li>
+                            <li class="list-group-item"><a href="pdf/Portaria_SF_procedimentos_contabeis_incentivador.pdf">Portaria SF nº 173/2018 - Procedimentos Contábeis</a></li>
+                            <li class="list-group-item"><a href="pdf/Portaria_SMC_prestacao_contas.pdf">Portaria Conjunta SMC/SF nº 92/2018 - Prestação de Contas</a></li>
+                            <li class="list-group-item"><a href="pdf/ANEXOS-PRESTACAO-DE-CONTAS-PROMAC.xlsx">Anexos da Portaria de Prestação de Contas</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.card-body -->
+
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Edital Vigente e Modelos</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <h6>Edital Vigente e Modelos</h6>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/Edital_PROMAC_2021.pdf" target="_blank">Edital Promac 2021</a></li>
+                            <li class="list-group-item"><a href="pdf/Comunicado_Prorrogacao_PROMAC 2021.pdf" target="_blank">Comunicado Prorrogação Promac 2021</a></li>
+                            <li class="list-group-item"><a href="pdf/DECLARAÇÃO_CCM_PF.docx" target="_blank">Declaração CCM - Pessoa Física</a></li>
+                            <li class="list-group-item"><a href="pdf/DECLARAÇÃO_CCM_PJ.docx" target="_blank">Declaração CCM - Pessoa Jurídica</a></li>
+                        </ul>
+                        <p><b>Modelos de Termos de Responsabilidade</b></p>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/MODELO_TERMO_DE_RESPONSABILIDADE_PROJETOS_APROVADOS_EM_2020_SEM_RECURSOS_EM_CONTA.docx" target="_blank">Projetos Aprovados em 2020 Sem Recursos em Conta</a></li>
+                            <li class="list-group-item"><a href="pdf/MODELO_TERMO_DE_RESPONSABILIDADE_PROJETOS_APROVADOS_EM_2020_COM_RECURSOS_EM_CONTA.docx" target="_blank">Projetos Aprovados em 2020 Com Recursos em Conta  <small>(Enviar extrato da conta juntamente com esse modelo preenchido)</small></a></li>
+                            <li class="list-group-item"><a href="pdf/MODELO_TERMO_DE_RESPONSABILIDADE_PROJETOS_APROVADOS_EM_2021_SEM_RECURSOS_EM_CONTA.docx" target="_blank">Projetos Aprovados em 2021 Sem Recursos em Conta</a></li>
+                        </ul>
+                        <p><b>Modelos de Solicitação de Alteração de Projeto</b></p>
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/MODELOS_DE_SOLICITACAO_DE_ALTERACAO_DE_PROJETO_PROMAC.pdf" target="_blank">Modelos de Solicitação de Alteração de Projeto</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Editais Anteriores</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/Edital_PROMAC_2020.pdf" target="_blank">Edital Promac 2020</a></li>
+                            <li class="list-group-item"><a href="pdf/Prorrogacao_Pro-Mac_2020.pdf" target="_blank">Prorrogação de Prazo de Inscrição</a></li>
+                            <li class="list-group-item"><a href="pdf/2° prorrogação de prazo de inscrição de projetos.pdf" target="_blank">2ª Prorrogação de Inscrição de Projetos - Edital PROMAC 2020</a></li>
+                            <li class="list-group-item"><a href="pdf/Edital_PROMAC_2018.pdf" target="_blank">Edital Promac 2018</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.card-body -->
+
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-md">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Manuais e Instruções</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/Mini_Manual_do_Incentivador_2021.pdf" target="_blank">Mini Manual do Incentivador 2021</a></li>
+                            <li class="list-group-item"><a href="pdf/Mapa_Equipamentos_Culturais_07-01-2019.pdf" target="_blank">Mapa de Equipamentos Culturais</a></li>
+                            <li class="list-group-item"><a href="pdf/EQUIPAMENTOS_CULTURAIS_FAIXAS_E_ENDERECOS.pdf" target="_blank">Lista de Equipamentos Culturais da SMC e CEUS com Endereços e Faixas de Renúncia Fiscal</a></li>
+                            <li class="list-group-item"><a href="pdf/COMO_USAR_O_GEOSAMPA_PARA_CONSULTAR_DISTRITO.pdf" target="_blank">Como usar o Geosampa para localizar o distrito de um endereço?</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <div class="col-md">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Normativos Revogado</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="pdf/Decreto_ProMac.pdf" target="_blank">Decreto nº 58.041/2017 (Revogado)</a></li>
+                            <li class="list-group-item"><a href="pdf/AlteracaoDecreto_ProMac.pdf" target="_blank"> Alteração do Decreto nº 58.041/2017 (Revogada)</a></li>
+                            <li class="list-group-item"><a href="pdf/Portaria_publicado.pdf">Portaria SMC nº 69/2018 - Portaria de Procedimentos (Revogada)</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.card-body -->
+
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
