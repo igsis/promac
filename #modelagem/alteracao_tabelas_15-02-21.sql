e50cb11093b264d09f9418a4f8efd2f03262bd84
Diego Vicente <34279091+DiegoVSantos@users.noreply.github.com>
2021-07-08 19:37:59 -0300

    remoção da pasta modelagem

--- #modelagem/alteracao_tabelas_15-02-21.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `locais_realizacao`
-    ADD COLUMN `observacaoLocal` TEXT NULL DEFAULT NULL AFTER `idDistrito`;
-
-ALTER TABLE `projeto`
-    CHANGE COLUMN `avaliaProjeto` `avaliaProjeto` TINYINT(1) NULL DEFAULT '0' AFTER `verificadoComissao`;
